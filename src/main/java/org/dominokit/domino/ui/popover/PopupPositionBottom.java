2281e3395d7e0d06732fcd7a478e32556bb09aad
Ahmad Bawaneh <akabme@gmail.com>
2018-04-04 08:50:54 +0300

    fix styles names

--- src/main/java/org/dominokit/domino/ui/popover/PopupPositionBottom.java
+++ src/main/java/org/dominokit/domino/ui/popover/PopupPositionBottom.java
@@ -4,12 +4,14 @@ import elemental2.dom.ClientRect;
 import elemental2.dom.DomGlobal;
 import elemental2.dom.HTMLElement;
 
+import static elemental2.dom.DomGlobal.*;
+
 public class PopupPositionBottom implements PopupPosition {
     @Override
     public void position(HTMLElement tooltip, HTMLElement target) {
         ClientRect targetRect = target.getBoundingClientRect();
         ClientRect tooltipRect = tooltip.getBoundingClientRect();
-        tooltip.style.setProperty("top", (targetRect.top+ DomGlobal.window.scrollY+targetRect.height)+"px");
+        tooltip.style.setProperty("top", (targetRect.top+ window.scrollY+targetRect.height)+"px");
         tooltip.style.setProperty("left", targetRect.left+((targetRect.width-tooltipRect.width)/2)+"px");
     }
 
