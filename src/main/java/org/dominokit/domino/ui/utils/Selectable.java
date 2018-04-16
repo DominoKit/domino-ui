cb21da972588ac8ded36ee7d64ec880c2a8abd7d
Ahmad Bawaneh <akabme@gmail.com>
2018-04-17 01:38:55 +0300

    enhacements

--- src/main/java/org/dominokit/domino/ui/utils/Selectable.java
+++ src/main/java/org/dominokit/domino/ui/utils/Selectable.java
@@ -3,5 +3,7 @@ package org.dominokit.domino.ui.utils;
 public interface Selectable<T> {
     T select();
     T deselect();
+    T select(boolean silent);
+    T deselect(boolean silent);
     boolean isSelected();
 }
