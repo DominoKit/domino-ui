206b2d8a6a572550ffad8979e132eed6a1eb9f5c
Ahmad Bawaneh <akabme@gmail.com>
2018-04-17 01:38:55 +0300

    enhacements

--- src/main/java/org/dominokit/domino/ui/lists/BaseListItem.java
+++ src/main/java/org/dominokit/domino/ui/lists/BaseListItem.java
@@ -1,28 +1,30 @@
 package org.dominokit.domino.ui.lists;
 
+import elemental2.dom.Element;
 import elemental2.dom.HTMLElement;
 import elemental2.dom.HTMLHeadingElement;
 import elemental2.dom.HTMLParagraphElement;
+import org.dominokit.domino.ui.utils.ElementUtil;
 
 import static java.util.Objects.isNull;
 import static org.jboss.gwt.elemento.core.Elements.h;
 import static org.jboss.gwt.elemento.core.Elements.p;
 
-public class BaseListItem {
+public class BaseListItem<T extends Element> {
 
-    private HTMLElement element;
+    private T element;
     private HTMLHeadingElement header;
     private HTMLParagraphElement body;
 
-    BaseListItem(HTMLElement element) {
+    BaseListItem(T element) {
         this.element = element;
     }
 
     void setHeaderText(String heading) {
         if (isNull(header)) {
             body = p().css("list-group-item-text").asElement();
-            body.textContent = element.textContent;
-            element.textContent = "";
+            body.innerHTML = element.innerHTML;
+            ElementUtil.clear(element);
             header = h(4).css("list-group-item-heading").asElement();
             header.textContent = heading;
             element.appendChild(header);
@@ -37,5 +39,9 @@ public class BaseListItem {
             element.textContent = content;
         else
             body.textContent = content;
+    }
+
+    public T getElement() {
+        return element;
     }
 }
