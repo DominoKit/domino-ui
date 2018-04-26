d287d92ba8bef664ef925b4e35365f7e2331a9ea
rjeeb <rafat.albarouki@gmail.com>
2018-04-26 21:35:58 +0300

    https://github.com/DominoKit/domino-ui/issues/13

---
+++ src/main/java/org/dominokit/domino/ui/upload/FileItem.java
@@ -0,0 +1,438 @@
+package org.dominokit.domino.ui.upload;
+
+import elemental2.core.JsNumber;
+import elemental2.dom.*;
+import org.dominokit.domino.ui.Typography.Paragraph;
+import org.dominokit.domino.ui.icons.Icons;
+import org.dominokit.domino.ui.popover.PopupPosition;
+import org.dominokit.domino.ui.popover.Tooltip;
+import org.dominokit.domino.ui.progress.Progress;
+import org.dominokit.domino.ui.progress.ProgressBar;
+import org.dominokit.domino.ui.style.Background;
+import org.dominokit.domino.ui.style.Color;
+import org.dominokit.domino.ui.thumbnails.Thumbnail;
+import org.jboss.gwt.elemento.core.Elements;
+import org.jboss.gwt.elemento.core.IsElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.jboss.gwt.elemento.core.Elements.h;
+
+public class FileItem implements IsElement<HTMLDivElement> {
+    private static final String[] UNITS = {"KB", "MB", "GB", "TB"};
+    private static final String ELLIPSIS_TEXT = "ellipsis-text";
+
+    private Thumbnail thumbnail = Thumbnail.create();
+    private FileImage fileImage;
+    private HTMLParagraphElement fileSizeParagraph;
+    private HTMLHeadingElement fileNameTitleContainer;
+    private HTMLDivElement footerContainer = Elements.div().asElement();
+    private HTMLElement deleteIcon = Icons.ALL.delete().asElement();
+    private HTMLElement messageContainer = Elements.p().css(ELLIPSIS_TEXT).asElement();
+    private HTMLDivElement progressElement;
+    private ProgressBar progressBar;
+    private HTMLElement cancelIcon = Icons.ALL.cancel().asElement();
+
+    private File file;
+    private UploadOptions options;
+
+    private List<RemoveFileHandler> removeHandlers = new ArrayList<>();
+    private List<ErrorHandler> errorHandlers = new ArrayList<>();
+    private List<ProgressHandler> progressHandlers = new ArrayList<>();
+    private List<BeforeUploadHandler> beforeUploadHandlers = new ArrayList<>();
+    private List<SuccessUploadHandler> successUploadHandlers = new ArrayList<>();
+    private List<CancelHandler> cancelHandlers = new ArrayList<>();
+    private String successMessage;
+    private String errorMessage;
+    private String unsentMessage = "Error while sending request";
+    private XMLHttpRequest request;
+    private boolean canceled;
+    private boolean uploaded;
+
+
+    public static FileItem create(File file, UploadOptions options) {
+        return new FileItem(file, options);
+    }
+
+    public FileItem(File file, UploadOptions options) {
+        this.file = file;
+        this.options = options;
+        initFileImage();
+        initFileTitle();
+        initFileSizeParagraph();
+        initFooter();
+        initProgress();
+        initThumbnail();
+
+        if (isExceedsMaxFile())
+            invalidate("File is too large, maximum file size is " + formatSize(options.getMaxFileSize()));
+    }
+
+    private void initFileImage() {
+        if (isImage())
+            fileImage = FileImage.createImageFile(file);
+        else
+            fileImage = FileImage.create();
+    }
+
+    public boolean isImage() {
+        return file.type.startsWith("image");
+    }
+
+    private void initFileTitle() {
+        fileNameTitleContainer = h(3).css(ELLIPSIS_TEXT).textContent(file.name).asElement();
+        fileNameTitleContainer.style.margin = CSSProperties.MarginUnionType.of("0px");
+        Tooltip.create(fileNameTitleContainer, file.name);
+    }
+
+    private void initFileSizeParagraph() {
+        fileSizeParagraph = Paragraph.create("File size : " + readableFileSize()).asElement();
+    }
+
+    private void initFooter() {
+        initDeleteIcon();
+        initCancelIcon();
+        initMessageContainer();
+        footerContainer.style.display = "inline-block";
+        footerContainer.style.width = CSSProperties.WidthUnionType.of("100%");
+        footerContainer.style.height = CSSProperties.HeightUnionType.of("25px");
+        footerContainer.appendChild(cancelIcon);
+        footerContainer.appendChild(deleteIcon);
+        footerContainer.appendChild(messageContainer);
+    }
+
+    private void initDeleteIcon() {
+        deleteIcon.style.cssFloat = "right";
+        deleteIcon.style.cursor = "pointer";
+        deleteIcon.addEventListener("click", evt -> remove());
+    }
+
+    private void initCancelIcon() {
+        cancelIcon.style.cssFloat = "right";
+        cancelIcon.style.cursor = "pointer";
+        cancelIcon.addEventListener("click", evt -> cancel());
+        cancelIcon.style.display = "none";
+    }
+
+    private void initMessageContainer() {
+        messageContainer.style.height = CSSProperties.HeightUnionType.of("20px");
+    }
+
+    private void initProgress() {
+        progressBar = ProgressBar.create(file.size);
+        progressElement = Progress.create()
+                .addBar(progressBar)
+                .asElement();
+        progressElement.style.marginBottom = CSSProperties.MarginBottomUnionType.of("0px");
+        progressElement.style.height = CSSProperties.HeightUnionType.of("5px");
+    }
+
+    private void initThumbnail() {
+        thumbnail.asElement().appendChild(fileImage.asElement());
+        thumbnail.appendCaptionContent(fileNameTitleContainer);
+        thumbnail.appendCaptionContent(fileSizeParagraph);
+        thumbnail.appendCaptionContent(footerContainer);
+        thumbnail.appendCaptionContent(progressElement);
+        thumbnail.getContentElement().style.cssText = "cursor: default !important";
+        thumbnail.getContentElement().style.padding = CSSProperties.PaddingUnionType.of("5px");
+        thumbnail.getContentElement().remove();
+    }
+
+
+    @Override
+    public HTMLDivElement asElement() {
+        return thumbnail.asElement();
+    }
+
+    private void setThumbnailBorder(Color red) {
+        thumbnail.asElement().style.border = "1px solid " + red.getHex();
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public FileItem setSizeTitle(String sizeTitle) {
+        fileSizeParagraph.textContent = sizeTitle;
+        return this;
+    }
+
+    public String readableFileSize() {
+        return formatSize(file.size);
+    }
+
+    private String formatSize(double size) {
+        int threshold = 1000;
+        if (Math.abs(size) < threshold)
+            return size + " B";
+
+        int unitIndex = -1;
+        do {
+            size /= threshold;
+            ++unitIndex;
+        } while (Math.abs(size) >= threshold && unitIndex < UNITS.length - 1);
+        return new JsNumber(size).toFixed(1) + " " + UNITS[unitIndex];
+    }
+
+    private void updateProgress(double loaded) {
+        progressBar.setValue(loaded);
+        progressHandlers.forEach(handler -> handler.onProgress(loaded, request));
+    }
+
+    public FileItem addRemoveHandler(RemoveFileHandler removeHandler) {
+        removeHandlers.add(removeHandler);
+        return this;
+    }
+
+    public FileItem addErrorHandler(ErrorHandler errorHandler) {
+        errorHandlers.add(errorHandler);
+        return this;
+    }
+
+    public FileItem addProgressHandler(ProgressHandler progressHandler) {
+        progressHandlers.add(progressHandler);
+        return this;
+    }
+
+    public FileItem addBeforeUploadHandler(BeforeUploadHandler beforeUploadHandler) {
+        beforeUploadHandlers.add(beforeUploadHandler);
+        return this;
+    }
+
+    public FileItem addSuccessUploadHandler(SuccessUploadHandler successUploadHandler) {
+        successUploadHandlers.add(successUploadHandler);
+        return this;
+    }
+
+    public FileItem addCancelHandler(CancelHandler cancelHandler) {
+        cancelHandlers.add(cancelHandler);
+        return this;
+    }
+
+    public void upload() {
+        if (!isExceedsMaxFile() && !uploaded) {
+            resetState();
+
+            request = new XMLHttpRequest();
+
+            request.upload.addEventListener("loadstart", evt -> {
+                cancelIcon.style.display = "inline-block";
+            });
+
+            request.upload.addEventListener("loadend", evt -> {
+                cancelIcon.style.display = "none";
+            });
+
+            request.upload.onprogress = p0 -> {
+                if (p0.lengthComputable)
+                    updateProgress(p0.loaded);
+            };
+
+            request.onabort = p0 -> {
+                resetProgress();
+                cancelHandlers.forEach(handler -> handler.onCancel(request));
+            };
+
+            request.addEventListener("readystatechange", evt -> {
+                if (request.readyState == 4) {
+                    if (request.status == 200)
+                        onSuccess();
+                    else if (!canceled)
+                        onError();
+                }
+            });
+            request.open("post", options.getUrl());
+            FormData formData = new FormData();
+            formData.append(file.name, file);
+            beforeUploadHandlers.forEach(handler -> handler.onBeforeUpload(request));
+            request.send(formData);
+        }
+    }
+
+    private void resetState() {
+        thumbnail.asElement().style.border = "1px solid #ddd";
+        messageContainer.textContent = "";
+        canceled = false;
+        updateProgress(0);
+        updateProgressBackground(Background.BLUE);
+    }
+
+    private boolean isExceedsMaxFile() {
+        return options.getMaxFileSize() > 0 && file.size > options.getMaxFileSize();
+    }
+
+    private void resetProgress() {
+        progressBar.setValue(0);
+    }
+
+    private void onSuccess() {
+        uploaded = true;
+        setThumbnailBorder(Color.GREEN);
+        updateProgressBackground(Background.GREEN);
+        updateProgress(file.size);
+        setMessage(getSuccessMessage(), Color.GREEN);
+        successUploadHandlers.forEach(handler -> handler.onSuccessUpload(request));
+    }
+
+    private String getSuccessMessage() {
+        return successMessage == null ? "Uploaded successfully" : successMessage;
+    }
+
+    private void onError() {
+        invalidate(getErrorMessage());
+        updateProgressBackground(Background.RED);
+        errorHandlers.forEach(handler -> handler.onError(request));
+    }
+
+    private String getErrorMessage() {
+        if (errorMessage != null)
+            return errorMessage;
+        return request.responseText.isEmpty() ? "Error while sending request" : request.responseText;
+    }
+
+    public void invalidate(String message) {
+        setThumbnailBorder(Color.RED);
+        setMessage(message, Color.RED);
+    }
+
+    private void setMessage(String message, Color color) {
+        messageContainer.textContent = message;
+        messageContainer.style.color = color.getHex();
+        Tooltip.create(messageContainer, message).position(PopupPosition.BOTTOM);
+    }
+
+    private void updateProgressBackground(Background background) {
+        progressBar.setBackground(background);
+    }
+
+    public void remove() {
+        asElement().remove();
+        removeHandlers.forEach(handler -> handler.onRemoveFile(file));
+    }
+
+    public FileImage getFileImage() {
+        return fileImage;
+    }
+
+    public HTMLParagraphElement getFileSizeParagraph() {
+        return fileSizeParagraph;
+    }
+
+    public HTMLHeadingElement getFileNameTitleContainer() {
+        return fileNameTitleContainer;
+    }
+
+    public HTMLDivElement getFooterContainer() {
+        return footerContainer;
+    }
+
+    public HTMLElement getDeleteIcon() {
+        return deleteIcon;
+    }
+
+    public HTMLElement getMessageContainer() {
+        return messageContainer;
+    }
+
+    public HTMLDivElement getProgressElement() {
+        return progressElement;
+    }
+
+    public ProgressBar getProgressBar() {
+        return progressBar;
+    }
+
+    public List<RemoveFileHandler> getRemoveHandlers() {
+        return removeHandlers;
+    }
+
+    public List<ErrorHandler> getErrorHandlers() {
+        return errorHandlers;
+    }
+
+    public List<ProgressHandler> getProgressHandlers() {
+        return progressHandlers;
+    }
+
+    public List<BeforeUploadHandler> getBeforeUploadHandlers() {
+        return beforeUploadHandlers;
+    }
+
+    public List<SuccessUploadHandler> getSuccessUploadHandlers() {
+        return successUploadHandlers;
+    }
+
+    public FileItem setSuccessUploadMessage(String successMessage) {
+        this.successMessage = successMessage;
+        return this;
+    }
+
+    public FileItem setErrorUploadMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+        return this;
+    }
+
+    public FileItem setUnsentMessage(String unsentMessage) {
+        this.unsentMessage = unsentMessage;
+        return this;
+    }
+
+    public FileItem cancel() {
+        if (request != null) {
+            canceled = true;
+            request.abort();
+        }
+        return this;
+    }
+
+    public HTMLElement getCancelIcon() {
+        return cancelIcon;
+    }
+
+    public List<CancelHandler> getCancelHandlers() {
+        return cancelHandlers;
+    }
+
+    public String getUnsentMessage() {
+        return unsentMessage;
+    }
+
+    public boolean isCanceled() {
+        return canceled;
+    }
+
+    public boolean isUploaded() {
+        return uploaded;
+    }
+
+    @FunctionalInterface
+    public interface RemoveFileHandler {
+        void onRemoveFile(File file);
+    }
+
+    @FunctionalInterface
+    public interface ErrorHandler {
+        void onError(XMLHttpRequest request);
+    }
+
+    @FunctionalInterface
+    public interface ProgressHandler {
+        void onProgress(double loaded, XMLHttpRequest request);
+    }
+
+    @FunctionalInterface
+    public interface BeforeUploadHandler {
+        void onBeforeUpload(XMLHttpRequest request);
+    }
+
+    @FunctionalInterface
+    public interface SuccessUploadHandler {
+        void onSuccessUpload(XMLHttpRequest request);
+    }
+
+    @FunctionalInterface
+    public interface CancelHandler {
+        void onCancel(XMLHttpRequest request);
+    }
+}
---
+++ src/main/java/org/dominokit/domino/ui/upload/FileUpload.java
@@ -0,0 +1,220 @@
+package org.dominokit.domino.ui.upload;
+
+import elemental2.dom.*;
+import org.dominokit.domino.ui.column.Column;
+import org.dominokit.domino.ui.icons.Icon;
+import org.dominokit.domino.ui.row.Row;
+import org.dominokit.domino.ui.utils.HasName;
+import org.jboss.gwt.elemento.core.Elements;
+import org.jboss.gwt.elemento.core.IsElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileUpload implements IsElement<HTMLDivElement>, HasName<FileUpload> {
+
+    private Row row = Row.create();
+    private Column column = Column.create()
+            .onLarge(Column.OnLarge.two)
+            .onMedium(Column.OnMedium.four)
+            .onSmall(Column.OnSmall.six)
+            .onXSmall(Column.OnXSmall.twelve);
+
+    private HTMLDivElement formElement = Elements.div().css("domino-file-upload").asElement();
+    private HTMLDivElement uploadMessageContainer = Elements.div().css("domino-file-upload-message").asElement();
+    private HTMLDivElement uploadIconContainer = Elements.div().css("domino-file-upload-message-icon").asElement();
+    private HTMLInputElement hiddenFileInput;
+    private HTMLDivElement filesContainer = Elements.div().asElement();
+    private List<FileItem> addedFileItems = new ArrayList<>();
+    private double maxFileSize;
+    private String url;
+
+    private List<OnAddFileHandler> onAddFileHandlers = new ArrayList<>();
+    private boolean autoUpload = true;
+
+    public FileUpload(Icon icon) {
+        uploadIconContainer.appendChild(icon.asElement());
+        uploadMessageContainer.appendChild(uploadIconContainer);
+        createHiddenInput();
+        formElement.appendChild(uploadMessageContainer);
+        formElement.appendChild(filesContainer);
+
+        hiddenFileInput.addEventListener("input", evt -> {
+            uploadFiles(hiddenFileInput.files);
+        });
+        uploadMessageContainer.addEventListener("click", evt -> hiddenFileInput.click());
+        formElement.addEventListener("drop", evt -> {
+            uploadFiles(((DragEvent) evt).dataTransfer.files);
+            removeHover();
+            evt.preventDefault();
+        });
+        formElement.addEventListener("dragover", evt -> {
+            addHover();
+            evt.preventDefault();
+        });
+        formElement.addEventListener("dragleave", evt -> {
+            removeHover();
+            evt.preventDefault();
+        });
+        filesContainer.appendChild(row.asElement());
+    }
+
+    private void addHover() {
+        formElement.classList.add("domino-file-upload-hover");
+    }
+
+    private void uploadFiles(FileList files) {
+        for (int i = 0; i < files.length; i++) {
+            File file = files.item(i);
+            addFilePreview(file);
+        }
+        if (autoUpload)
+            uploadAllFiles();
+    }
+
+    public void uploadAllFiles() {
+        addedFileItems.forEach(FileItem::upload);
+    }
+
+    private void addFilePreview(File file) {
+        FileItem fileItem = FileItem.create(file, new UploadOptions(url, maxFileSize));
+        Column previewColumn = column.copy().addElement(fileItem.asElement());
+
+        fileItem.addRemoveHandler(removedFile -> {
+            previewColumn.asElement().remove();
+            addedFileItems.remove(fileItem);
+        });
+
+        addedFileItems.add(fileItem);
+        row.addColumn(previewColumn);
+        onAddFileHandlers.forEach(handler -> handler.onAddFile(fileItem));
+    }
+
+    private void removeHover() {
+        formElement.classList.remove("domino-file-upload-hover");
+    }
+
+    private void createHiddenInput() {
+        hiddenFileInput = Elements.input("file")
+                .style("visibility: hidden; position: absolute; top: 0px; left: 0px; height: 0px; width: 0px;").asElement();
+        DomGlobal.document.body.appendChild(hiddenFileInput);
+    }
+
+    public static FileUpload create(Icon icon) {
+        return new FileUpload(icon);
+    }
+
+    @Override
+    public HTMLDivElement asElement() {
+        return formElement;
+    }
+
+    public FileUpload appendChild(Node child) {
+        uploadMessageContainer.appendChild(child);
+        return this;
+    }
+
+    public FileUpload multipleFiles() {
+        hiddenFileInput.multiple = true;
+        return this;
+    }
+
+    public FileUpload singleFile() {
+        hiddenFileInput.multiple = false;
+        return this;
+    }
+
+    public FileUpload accept(String acceptedFiles) {
+        hiddenFileInput.accept = acceptedFiles;
+        return this;
+    }
+
+    public FileUpload maxFileSize(double maxFileSize) {
+        this.maxFileSize = maxFileSize;
+        return this;
+    }
+
+    public FileUpload setUrl(String url) {
+        this.url = url;
+        return this;
+    }
+
+    public FileUpload addOnAddFileHandler(OnAddFileHandler onAddFileHandler) {
+        onAddFileHandlers.add(onAddFileHandler);
+        return this;
+    }
+
+    public FileUpload autoUpload() {
+        this.autoUpload = true;
+        return this;
+    }
+
+    public FileUpload manualUpload() {
+        this.autoUpload = false;
+        return this;
+    }
+
+    public Row getRow() {
+        return row;
+    }
+
+    public Column getColumn() {
+        return column;
+    }
+
+    public HTMLDivElement getFormElement() {
+        return formElement;
+    }
+
+    public HTMLDivElement getUploadMessageContainer() {
+        return uploadMessageContainer;
+    }
+
+    public HTMLDivElement getUploadIconContainer() {
+        return uploadIconContainer;
+    }
+
+    public HTMLInputElement getHiddenFileInput() {
+        return hiddenFileInput;
+    }
+
+    public HTMLDivElement getFilesContainer() {
+        return filesContainer;
+    }
+
+    public List<FileItem> getAddedFileItems() {
+        return addedFileItems;
+    }
+
+    public double getMaxFileSize() {
+        return maxFileSize;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public List<OnAddFileHandler> getOnAddFileHandlers() {
+        return onAddFileHandlers;
+    }
+
+    public boolean isAutoUpload() {
+        return autoUpload;
+    }
+
+    @Override
+    public String getName() {
+        return hiddenFileInput.name;
+    }
+
+    @Override
+    public FileUpload setName(String name) {
+        hiddenFileInput.name = name;
+        return this;
+    }
+
+    @FunctionalInterface
+    public interface OnAddFileHandler {
+        void onAddFile(FileItem fileItem);
+    }
+}
