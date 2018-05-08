b858ee342f79597d0190c1c010d2035f1e793c9e
Ahmad K. Bawaneh <akabme@gmail.com>
2018-05-08 10:09:02 +0300

    Time picker (#18)
    * enhacements
    
    * fix theme issue

--- src/main/java/org/dominokit/domino/ui/themes/Theme.java
+++ src/main/java/org/dominokit/domino/ui/themes/Theme.java
@@ -1,11 +1,56 @@
 package org.dominokit.domino.ui.themes;
 
+
 import org.dominokit.domino.ui.style.ColorScheme;
 
 import static elemental2.dom.DomGlobal.document;
 import static java.util.Objects.nonNull;
 
 public class Theme {
+
+    public static final ColorScheme RED=ColorScheme.RED;
+
+    public static final ColorScheme PINK=ColorScheme.PINK;
+
+    public static final ColorScheme PURPLE=ColorScheme.PURPLE;
+
+    public static final ColorScheme DEEP_PURPLE=ColorScheme.DEEP_PURPLE;
+
+    public static final ColorScheme INDIGO=ColorScheme.INDIGO;
+
+    public static final ColorScheme BLUE=ColorScheme.BLUE;
+
+    public static final ColorScheme LIGHT_BLUE=ColorScheme.LIGHT_BLUE;
+
+    public static final ColorScheme CYAN=ColorScheme.CYAN;
+
+    public static final ColorScheme TEAL=ColorScheme.TEAL;
+
+    public static final ColorScheme GREEN=ColorScheme.GREEN;
+
+    public static final ColorScheme LIGHT_GREEN=ColorScheme.LIGHT_GREEN;
+
+    public static final ColorScheme LIME=ColorScheme.LIME;
+
+    public static final ColorScheme YELLOW=ColorScheme.YELLOW;
+
+    public static final ColorScheme AMBER=ColorScheme.AMBER;
+
+    public static final ColorScheme ORANGE=ColorScheme.ORANGE;
+
+    public static final ColorScheme DEEP_ORANGE=ColorScheme.DEEP_ORANGE;
+
+    public static final ColorScheme BROWN=ColorScheme.BROWN;
+
+    public static final ColorScheme GREY=ColorScheme.GREY;
+
+    public static final ColorScheme BLUE_GREY=ColorScheme.BLUE_GREY;
+
+    public static final ColorScheme BLACK=ColorScheme.BLACK;
+
+    public static final ColorScheme WHITE=ColorScheme.WHITE;
+
+    public static final ColorScheme TRANSPARENT=ColorScheme.TRANSPARENT;
 
     private final ColorScheme scheme;
     private final String themeStyle;
