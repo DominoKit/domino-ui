e4865ca378602d5acc24028caf8c9b7e51019fb9
rjeeb <rafat.albarouki@gmail.com>
2018-06-03 00:11:54 +0300

    https://github.com/DominoKit/domino-ui/issues/34

--- src/main/java/org/dominokit/domino/ui/button/DropdownButton.java
+++ src/main/java/org/dominokit/domino/ui/button/DropdownButton.java
@@ -1,44 +1,55 @@
 package org.dominokit.domino.ui.button;
 
-import org.dominokit.domino.ui.button.group.ButtonsGroup;
-import org.dominokit.domino.ui.style.Color;
-import org.dominokit.domino.ui.style.StyleType;
-import org.dominokit.domino.ui.utils.HasContent;
-import org.dominokit.domino.ui.utils.Justifiable;
 import elemental2.dom.*;
 import jsinterop.base.Js;
+import org.dominokit.domino.ui.button.group.ButtonsGroup;
+import org.dominokit.domino.ui.icons.Icon;
+import org.dominokit.domino.ui.style.Color;
+import org.dominokit.domino.ui.style.StyleType;
+import org.dominokit.domino.ui.utils.HasBackground;
+import org.dominokit.domino.ui.utils.HasContent;
+import org.dominokit.domino.ui.utils.Justifiable;
 import org.jboss.gwt.elemento.core.Elements;
 
 import java.util.LinkedList;
 import java.util.List;
 
-public class DropdownButton implements Justifiable, HasContent<DropdownButton> {
+import static java.util.Objects.nonNull;
 
+public class DropdownButton implements Justifiable, HasContent<DropdownButton>, HasBackground<DropdownButton> {
+
+    private HTMLElement caret = Elements.span().css("caret").asElement();
     private HTMLElement groupElement = ButtonsGroup.create().asElement();
     private HTMLUListElement actionsElement = Elements.ul().css("dropdown-menu").asElement();
-    private Button button;
     private List<Justifiable> items = new LinkedList<>();
+    private Button button;
+    private Color background;
 
-    private DropdownButton(String content, StyleType type) {
+    public DropdownButton(String content, StyleType type) {
         this(Button.create(content).setButtonType(type));
     }
 
-    private DropdownButton(String content, Color background) {
+    public DropdownButton(String content, Color background) {
         this(Button.create(content).setBackground(background));
     }
 
-    private DropdownButton(String content) {
+    public DropdownButton(String content) {
         this(Button.create(content));
     }
 
-    private DropdownButton(Button button) {
-        this.button = button;
-        configureButton();
+    public DropdownButton(Icon icon, StyleType type) {
+        this(IconButton.create(icon).setButtonType(type));
     }
 
-    private void configureButton() {
+    public DropdownButton(Icon icon) {
+        this(IconButton.create(icon));
+    }
+
+    public DropdownButton(Button button) {
+        this.button = button;
         addHideListener();
-        groupElement.appendChild(asDropDown(button.asElement(), groupElement));
+        groupElement.appendChild(asDropDown(this.button.asElement(), groupElement));
+        this.button.asElement().appendChild(caret);
         groupElement.appendChild(actionsElement);
     }
 
@@ -55,7 +66,6 @@ public class DropdownButton implements Justifiable, HasContent<DropdownButton> {
         buttonElement.setAttribute("data-toggle", "dropdown");
         buttonElement.setAttribute("aria-haspopup", true);
         buttonElement.setAttribute("aria-expanded", true);
-        buttonElement.appendChild(Elements.span().css("caret").asElement());
         buttonElement.setAttribute("type", "button");
         buttonElement.addEventListener("click", event -> {
             closeAllGroups();
@@ -84,6 +94,80 @@ public class DropdownButton implements Justifiable, HasContent<DropdownButton> {
 
     private void close(Element item) {
         item.classList.remove("open");
+    }
+
+    public DropdownButton addAction(DropdownAction action) {
+        items.add(action);
+        actionsElement.appendChild(action.asElement());
+        return this;
+    }
+
+    @Override
+    public HTMLElement asElement() {
+        return groupElement;
+    }
+
+    public DropdownButton separator() {
+        JustifiableSeparator justifiableSeparator = new JustifiableSeparator();
+        items.add(justifiableSeparator);
+        actionsElement.appendChild(justifiableSeparator.asElement());
+        return this;
+    }
+
+    @Override
+    public HTMLElement justify() {
+        Button button = Button.create(this.button.asElement().textContent);
+
+        for (String style : this.button.asElement().classList.asList()) {
+            button.asElement().classList.add(style);
+        }
+
+        DropdownButton cloneDropdownButton = new DropdownButton(button);
+        for (Justifiable item : items) {
+            cloneDropdownButton.actionsElement.appendChild(item.justify());
+        }
+        return cloneDropdownButton.asElement();
+    }
+
+    @Override
+    public DropdownButton setContent(String content) {
+        button.setContent(content);
+        return this;
+    }
+
+    public DropdownButton dropup() {
+        groupElement.classList.add("dropup");
+        return this;
+    }
+
+    public DropdownButton dropdown() {
+        groupElement.classList.remove("dropup");
+        return this;
+    }
+
+    public DropdownButton hideCaret() {
+        if (isCaretAdded())
+            caret.remove();
+        return this;
+    }
+
+    public DropdownButton showCaret() {
+        if (!isCaretAdded())
+            button.asElement().appendChild(caret);
+        return this;
+    }
+
+    private boolean isCaretAdded() {
+        return button.asElement().contains(caret);
+    }
+
+    @Override
+    public DropdownButton setBackground(Color background) {
+        if (nonNull(this.background))
+            button.asElement().classList.remove(this.background.getBackground());
+        button.asElement().classList.add(background.getBackground());
+        this.background = background;
+        return this;
     }
 
     public static DropdownButton create(String content) {
@@ -122,58 +206,48 @@ public class DropdownButton implements Justifiable, HasContent<DropdownButton> {
         return create(content, StyleType.DANGER);
     }
 
-    public DropdownButton addAction(DropdownAction action) {
-        items.add(action);
-        actionsElement.appendChild(action.asElement());
-        return this;
+    public static DropdownButton create(Icon icon, StyleType type) {
+        return new DropdownButton(icon, type);
     }
 
-    @Override
-    public HTMLElement asElement() {
-        return groupElement;
+    public static DropdownButton create(Icon icon) {
+        return new DropdownButton(icon);
     }
 
-    public DropdownButton separator() {
-        JustifiableSeparator justifiableSeparator = new JustifiableSeparator();
-        items.add(justifiableSeparator);
-        actionsElement.appendChild(justifiableSeparator.asElement());
-        return this;
+    public static DropdownButton createDefault(Icon icon) {
+        return create(icon, StyleType.DEFAULT);
     }
 
-    @Override
-    public HTMLElement justify() {
-        Button button = Button.create(this.button.asElement().textContent);
-
-        for (String style : this.button.asElement().classList.asList()) {
-            button.asElement().classList.add(style);
-        }
-
-        DropdownButton cloneDropdownButton = new DropdownButton(button);
-        for (Justifiable item : items) {
-            cloneDropdownButton.actionsElement.appendChild(item.justify());
-        }
-        return cloneDropdownButton.asElement();
+    public static DropdownButton createPrimary(Icon icon) {
+        return create(icon, StyleType.PRIMARY);
     }
 
-    public DropdownButton setButtonType(StyleType type) {
-        button.setButtonType(type);
-        return this;
+    public static DropdownButton createSuccess(Icon icon) {
+        return create(icon, StyleType.SUCCESS);
     }
 
-    @Override
-    public DropdownButton setContent(String content) {
-        button.setContent(content);
-        return this;
+    public static DropdownButton createInfo(Icon icon) {
+        return create(icon, StyleType.INFO);
     }
 
-    public DropdownButton dropup() {
-        groupElement.classList.add("dropup");
-        return this;
+    public static DropdownButton createWarning(Icon icon) {
+        return create(icon, StyleType.WARNING);
     }
 
-    public DropdownButton dropdown() {
-        groupElement.classList.remove("dropup");
-        return this;
+    public static DropdownButton createDanger(Icon icon) {
+        return create(icon, StyleType.DANGER);
+    }
+
+    public HTMLElement getCaret() {
+        return caret;
+    }
+
+    public HTMLUListElement getActionsElement() {
+        return actionsElement;
+    }
+
+    public Button getButton() {
+        return button;
     }
 
     private class JustifiableSeparator implements Justifiable {
