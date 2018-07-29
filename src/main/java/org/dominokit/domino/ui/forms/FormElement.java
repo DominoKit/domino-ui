6c773a6e92f7bc1fc3bd9029968ca790aa40db4a
rjeeb <rafat.albarouki@gmail.com>
2018-07-29 11:31:20 +0300

    modify readonly to work only with valuebox and select

--- src/main/java/org/dominokit/domino/ui/forms/FormElement.java
+++ src/main/java/org/dominokit/domino/ui/forms/FormElement.java
@@ -3,7 +3,7 @@ package org.dominokit.domino.ui.forms;
 import org.dominokit.domino.ui.utils.*;
 
 public interface FormElement<T, V> extends HasName<T>, HasValue<T, V>, Switchable<T>,
-        HasHelperText<T>, HasLabel<T>, HasValidation<T>, HasAutoValidation<T>, IsRequired<T>, IsReadOnly<T> {
+        HasHelperText<T>, HasLabel<T>, HasValidation<T>, HasAutoValidation<T>, IsRequired<T> {
 
     boolean isEmpty();
 
