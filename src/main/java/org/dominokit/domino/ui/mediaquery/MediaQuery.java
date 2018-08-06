06ecc112fe9e5d16624fb948cb819ead8cb1708f
Ahmad Bawaneh <akabme@gmail.com>
2018-08-06 17:31:03 +0300

    https://github.com/DominoKit/domino-ui/issues/55

--- src/main/java/org/dominokit/domino/ui/mediaquery/MediaQuery.java
+++ src/main/java/org/dominokit/domino/ui/mediaquery/MediaQuery.java
@@ -18,11 +18,36 @@ public class MediaQuery {
     private static final String SMALL_MEDIA_QUERY = "(min-width: 768px) and (max-width: 992px)";
     private static final String XSMALL_MEDIA_QUERY = "(max-width: 768px)";
 
+    private static final String XLARGE_AND_UP_MEDIA_QUERY = "(min-width: 1800px)";
+    private static final String LARGE_AND_UP_MEDIA_QUERY = "(min-width: 1200px)";
+    private static final String MEDIUM_AND_UP_MEDIA_QUERY = "(min-width: 992px)";
+    private static final String SMALL_AND_UP_MEDIA_QUERY = "(min-width: 768px)";
+    private static final String XSMALL_AND_UP_MEDIA_QUERY = "(min-width: 0x)";
+
+    private static final String XLARGE_AND_DOWN_MEDIA_QUERY = "(max-width: 1800px)";
+    private static final String LARGE_AND_DOWN_MEDIA_QUERY = "(max-width: 1800px)";
+    private static final String MEDIUM_AND_DPOWN_MEDIA_QUERY = "(max-width: 1200px)";
+    private static final String SMALL_AND_DOWN_MEDIA_QUERY = "(max-width: 992px)";
+    private static final String XSMALL_AND_DOWN_MEDIA_QUERY = "(max-width: 768px)";
+
     private static MediaQueryList xlargeMediaQueryList = window.matchMedia(XLARGE_MEDIA_QUERY);
     private static MediaQueryList largeMediaQueryList = window.matchMedia(LARGE_MEDIA_QUERY);
     private static MediaQueryList mediumMediaQueryList = window.matchMedia(MEDIUM_MEDIA_QUERY);
     private static MediaQueryList smallMediaQueryList = window.matchMedia(SMALL_MEDIA_QUERY);
     private static MediaQueryList xsmallMediaQueryList = window.matchMedia(XSMALL_MEDIA_QUERY);
+
+    private static MediaQueryList xlargeAndUpMediaQueryList = window.matchMedia(XLARGE_AND_UP_MEDIA_QUERY);
+    private static MediaQueryList largeAndUpMediaQueryList = window.matchMedia(LARGE_AND_UP_MEDIA_QUERY);
+    private static MediaQueryList mediumAndUpMediaQueryList = window.matchMedia(MEDIUM_AND_UP_MEDIA_QUERY);
+    private static MediaQueryList smallAndUpMediaQueryList = window.matchMedia(SMALL_AND_UP_MEDIA_QUERY);
+    private static MediaQueryList xsmallAndUpMediaQueryList = window.matchMedia(XSMALL_AND_UP_MEDIA_QUERY);
+
+    private static MediaQueryList xlargeAndDownMediaQueryList = window.matchMedia(XLARGE_AND_DOWN_MEDIA_QUERY);
+    private static MediaQueryList largeAndDownMediaQueryList = window.matchMedia(LARGE_AND_DOWN_MEDIA_QUERY);
+    private static MediaQueryList mediumAndDownMediaQueryList = window.matchMedia(MEDIUM_AND_DPOWN_MEDIA_QUERY);
+    private static MediaQueryList smallAndDownMediaQueryList = window.matchMedia(SMALL_AND_DOWN_MEDIA_QUERY);
+    private static MediaQueryList xsmallAndDownMediaQueryList = window.matchMedia(XSMALL_AND_DOWN_MEDIA_QUERY);
+
 
     private static Map<MediaQueryListener, MediaQueryListListener> listenersMap = new HashMap<>();
     private static Map<String, MediaQueryList> mediaQueries = new HashMap<>();
@@ -47,12 +72,52 @@ public class MediaQuery {
         addListener(listener, xsmallMediaQueryList);
     }
 
+    public static void addOnXLargeAndUpListener(MediaQueryListener listener) {
+        addListener(listener, xlargeAndUpMediaQueryList);
+    }
+
+    public static void addOnLargeAndUpListener(MediaQueryListener listener) {
+        addListener(listener, largeAndUpMediaQueryList);
+    }
+
+    public static void addOnMediumAndUpListener(MediaQueryListener listener) {
+        addListener(listener, mediumAndUpMediaQueryList);
+    }
+
+    public static void addOnSmallAndUpListener(MediaQueryListener listener) {
+        addListener(listener, smallAndUpMediaQueryList);
+    }
+
+    public static void addOnXSmallAndUpListener(MediaQueryListener listener) {
+        addListener(listener, xsmallAndUpMediaQueryList);
+    }
+
     public static void addPortraitOrientationListener(MediaQueryListener listener) {
         addCustomQueryListener("(orientation : portrait)", listener);
     }
 
     public static void addLandscapeOrientationListener(MediaQueryListener listener) {
         addCustomQueryListener("(orientation : landscape)", listener);
+    }
+
+    public static void addOnXLargeAndDownListener(MediaQueryListener listener) {
+        addListener(listener, xlargeAndDownMediaQueryList);
+    }
+
+    public static void addOnLargeAndDownListener(MediaQueryListener listener) {
+        addListener(listener, largeAndDownMediaQueryList);
+    }
+
+    public static void addOnMediumAndDownListener(MediaQueryListener listener) {
+        addListener(listener, mediumAndDownMediaQueryList);
+    }
+
+    public static void addOnSmallAndDownListener(MediaQueryListener listener) {
+        addListener(listener, smallAndDownMediaQueryList);
+    }
+
+    public static void addOnXSmallAndDownListener(MediaQueryListener listener) {
+        addListener(listener, xsmallAndDownMediaQueryList);
     }
 
     private static void addListener(MediaQueryListener listener, MediaQueryList mediaQueryList) {
@@ -84,6 +149,47 @@ public class MediaQuery {
 
     public static void removeOnXSmallListener(MediaQueryListener listener) {
         removeListener(listener, xsmallMediaQueryList);
+    }
+
+    public static void removeOnXLargeAndUpListener(MediaQueryListener listener) {
+        removeListener(listener, xlargeAndUpMediaQueryList);
+    }
+
+    public static void removeOnLargeAndUpListener(MediaQueryListener listener) {
+        removeListener(listener, largeAndUpMediaQueryList);
+    }
+
+    public static void removeOnMediumAndUpListener(MediaQueryListener listener) {
+        removeListener(listener, mediumAndUpMediaQueryList);
+    }
+
+    public static void removeOnSmallAndUpListener(MediaQueryListener listener) {
+        removeListener(listener, smallAndUpMediaQueryList);
+    }
+
+    public static void removeOnXSmallAndUpListener(MediaQueryListener listener) {
+        removeListener(listener, xsmallMediaQueryList);
+    }
+
+
+    public static void removeOnXLargeAndDownListener(MediaQueryListener listener) {
+        removeListener(listener, xlargeAndDownMediaQueryList);
+    }
+
+    public static void removeOnLargeAndDownListener(MediaQueryListener listener) {
+        removeListener(listener, largeAndDownMediaQueryList);
+    }
+
+    public static void removeOnMediumAndDownListener(MediaQueryListener listener) {
+        removeListener(listener, mediumAndDownMediaQueryList);
+    }
+
+    public static void removeOnSmallAndDownListener(MediaQueryListener listener) {
+        removeListener(listener, smallAndDownMediaQueryList);
+    }
+
+    public static void removeOnXSmallAndDownListener(MediaQueryListener listener) {
+        removeListener(listener, xsmallAndDownMediaQueryList);
     }
 
     private static void removeListener(MediaQueryListener listener, MediaQueryList mediaQueryList) {
