e6205bd95cca324a0f8be9af99452f0dc9d1550c
Ahmad Bawaneh <akabme@gmail.com>
2018-08-08 14:41:39 +0300

    add helper methods and utitlies

--- src/main/java/org/dominokit/domino/ui/themes/Theme.java
+++ src/main/java/org/dominokit/domino/ui/themes/Theme.java
@@ -3,54 +3,59 @@ package org.dominokit.domino.ui.themes;
 
 import org.dominokit.domino.ui.style.ColorScheme;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static elemental2.dom.DomGlobal.document;
 import static java.util.Objects.nonNull;
 
 public class Theme {
 
-    public static final ColorScheme RED=ColorScheme.RED;
+    private static List<ThemeChangeHandler> themeChangeHandlers = new ArrayList<>();
 
-    public static final ColorScheme PINK=ColorScheme.PINK;
+    public static final ColorScheme RED = ColorScheme.RED;
 
-    public static final ColorScheme PURPLE=ColorScheme.PURPLE;
+    public static final ColorScheme PINK = ColorScheme.PINK;
 
-    public static final ColorScheme DEEP_PURPLE=ColorScheme.DEEP_PURPLE;
+    public static final ColorScheme PURPLE = ColorScheme.PURPLE;
 
-    public static final ColorScheme INDIGO=ColorScheme.INDIGO;
+    public static final ColorScheme DEEP_PURPLE = ColorScheme.DEEP_PURPLE;
 
-    public static final ColorScheme BLUE=ColorScheme.BLUE;
+    public static final ColorScheme INDIGO = ColorScheme.INDIGO;
 
-    public static final ColorScheme LIGHT_BLUE=ColorScheme.LIGHT_BLUE;
+    public static final ColorScheme BLUE = ColorScheme.BLUE;
 
-    public static final ColorScheme CYAN=ColorScheme.CYAN;
+    public static final ColorScheme LIGHT_BLUE = ColorScheme.LIGHT_BLUE;
 
-    public static final ColorScheme TEAL=ColorScheme.TEAL;
+    public static final ColorScheme CYAN = ColorScheme.CYAN;
 
-    public static final ColorScheme GREEN=ColorScheme.GREEN;
+    public static final ColorScheme TEAL = ColorScheme.TEAL;
 
-    public static final ColorScheme LIGHT_GREEN=ColorScheme.LIGHT_GREEN;
+    public static final ColorScheme GREEN = ColorScheme.GREEN;
 
-    public static final ColorScheme LIME=ColorScheme.LIME;
+    public static final ColorScheme LIGHT_GREEN = ColorScheme.LIGHT_GREEN;
 
-    public static final ColorScheme YELLOW=ColorScheme.YELLOW;
+    public static final ColorScheme LIME = ColorScheme.LIME;
 
-    public static final ColorScheme AMBER=ColorScheme.AMBER;
+    public static final ColorScheme YELLOW = ColorScheme.YELLOW;
 
-    public static final ColorScheme ORANGE=ColorScheme.ORANGE;
+    public static final ColorScheme AMBER = ColorScheme.AMBER;
 
-    public static final ColorScheme DEEP_ORANGE=ColorScheme.DEEP_ORANGE;
+    public static final ColorScheme ORANGE = ColorScheme.ORANGE;
 
-    public static final ColorScheme BROWN=ColorScheme.BROWN;
+    public static final ColorScheme DEEP_ORANGE = ColorScheme.DEEP_ORANGE;
 
-    public static final ColorScheme GREY=ColorScheme.GREY;
+    public static final ColorScheme BROWN = ColorScheme.BROWN;
 
-    public static final ColorScheme BLUE_GREY=ColorScheme.BLUE_GREY;
+    public static final ColorScheme GREY = ColorScheme.GREY;
 
-    public static final ColorScheme BLACK=ColorScheme.BLACK;
+    public static final ColorScheme BLUE_GREY = ColorScheme.BLUE_GREY;
 
-    public static final ColorScheme WHITE=ColorScheme.WHITE;
+    public static final ColorScheme BLACK = ColorScheme.BLACK;
 
-    public static final ColorScheme TRANSPARENT=ColorScheme.TRANSPARENT;
+    public static final ColorScheme WHITE = ColorScheme.WHITE;
+
+    public static final ColorScheme TRANSPARENT = ColorScheme.TRANSPARENT;
 
     private final ColorScheme scheme;
     private final String themeStyle;
@@ -60,8 +65,8 @@ public class Theme {
 
     public Theme(ColorScheme scheme) {
         this.scheme = scheme;
-        this.themeStyle=scheme.color().getStyle().replace("col-","theme-");
-        this.name=scheme.color().getName().replace(" ","_").toLowerCase();
+        this.themeStyle = scheme.color().getStyle().replace("col-", "theme-");
+        this.name = scheme.color().getName().replace(" ", "_").toLowerCase();
     }
 
     public ColorScheme getScheme() {
@@ -76,10 +81,25 @@ public class Theme {
         return name;
     }
 
+    public static void addThemeChangeHandler(ThemeChangeHandler themeChangeHandler) {
+        themeChangeHandlers.add(themeChangeHandler);
+    }
+
+    public static void removeThemeChangeHandler(ThemeChangeHandler themeChangeHandler) {
+        themeChangeHandlers.remove(themeChangeHandler);
+    }
+
     public void apply() {
+        Theme oldTheme = currentTheme;
         if (nonNull(currentTheme))
             document.body.classList.remove(currentTheme.themeStyle);
         this.currentTheme = this;
         document.body.classList.add(themeStyle);
+        themeChangeHandlers.forEach(themeChangeHandler -> themeChangeHandler.onThemeChanged(oldTheme, this));
+    }
+
+    @FunctionalInterface
+    public interface ThemeChangeHandler {
+        void onThemeChanged(Theme oldTheme, Theme newTheme);
     }
 }
