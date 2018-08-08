7c989384aa3d5c8cb17f4954d614e6c011302a7b
rjeeb <rafat.albarouki@gmail.com>
2018-08-08 11:43:04 +0300

    implementing footer in the layout

---
+++ src/main/java/org/dominokit/domino/ui/layout/Footer.java
@@ -0,0 +1,42 @@
+package org.dominokit.domino.ui.layout;
+
+import elemental2.dom.HTMLElement;
+import org.jboss.gwt.elemento.core.IsElement;
+
+import static org.jboss.gwt.elemento.core.Elements.footer;
+
+public class Footer implements IsElement<HTMLElement> {
+
+    private HTMLElement element = footer().css("footer").asElement();
+
+    public static Footer create() {
+        return new Footer();
+    }
+
+    public Footer() {
+        hide();
+    }
+
+    @Override
+    public HTMLElement asElement() {
+        return element;
+    }
+
+    public Footer fixed() {
+        element.classList.add("fixed");
+        return this;
+    }
+
+    public Footer unfixed() {
+        element.classList.remove("fixed");
+        return this;
+    }
+
+    public void hide() {
+        asElement().style.display = "none";
+    }
+
+    public void show() {
+        asElement().style.display = "block";
+    }
+}
