051f17f4a7185512639fca9eec8be7b796f2cf28
rjeeb <rafat.albarouki@gmail.com>
2018-09-20 14:08:07 +0300

    https://github.com/DominoKit/domino-ui/issues/116

---
+++ src/main/java/org/dominokit/domino/ui/grid/flex/FlexLayout.java
@@ -0,0 +1,57 @@
+package org.dominokit.domino.ui.grid.flex;
+
+import elemental2.dom.HTMLDivElement;
+import org.dominokit.domino.ui.utils.BaseDominoElement;
+import org.dominokit.domino.ui.utils.DominoElement;
+
+import static org.jboss.gwt.elemento.core.Elements.div;
+
+public class FlexLayout extends BaseDominoElement<HTMLDivElement, FlexLayout> {
+
+    private DominoElement<HTMLDivElement> element = DominoElement.of(div().css("flex-layout"));
+
+    public FlexLayout() {
+        init(this);
+    }
+
+    public static FlexLayout create() {
+        return new FlexLayout();
+    }
+
+    public FlexLayout setDirection(FlexDirection direction) {
+        element.style().setProperty("flex-direction", direction.getStyle());
+        return this;
+    }
+
+    public FlexLayout setWrap(FlexWrap wrap) {
+        element.style().setProperty("flex-wrap", wrap.getStyle());
+        return this;
+    }
+
+    public FlexLayout setFlow(FlexDirection direction, FlexWrap wrap) {
+        setDirection(direction);
+        setWrap(wrap);
+        return this;
+    }
+
+    public FlexLayout setJustifyContent(FlexJustifyContent justifyContent) {
+        element.style().setProperty("justify-content", justifyContent.getStyle());
+        return this;
+    }
+
+    public FlexLayout setAlignItems(FlexAlign alignItems) {
+        element.style().setProperty("align-items", alignItems.getStyle());
+        return this;
+    }
+
+    public FlexLayout appendChild(FlexItem flexItem) {
+        appendChild(flexItem.asElement());
+        return this;
+    }
+
+    @Override
+    public HTMLDivElement asElement() {
+        return element.asElement();
+    }
+
+}
