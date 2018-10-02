bd1fd439314e7688a6879922e2d0236be2d5f57f
rjeeb <rafat.albarouki@gmail.com>
2018-10-02 10:35:53 +0300

    https://github.com/DominoKit/domino-ui/issues/115

--- src/main/java/org/dominokit/domino/ui/notifications/BottomCenterPosition.java
+++ src/main/java/org/dominokit/domino/ui/notifications/BottomCenterPosition.java
@@ -12,7 +12,8 @@ public class BottomCenterPosition extends NotificationPosition {
     public void onBeforePosition(HTMLElement element) {
         element.style.setProperty("bottom", "20px");
         element.style.setProperty("right", "0px");
-        element.style.setProperty("left", "0px");
+        element.style.setProperty("left", "50%");
+        element.style.setProperty("transform", "translate(-50%)");
     }
 
     @Override
--- src/main/java/org/dominokit/domino/ui/notifications/TopCenterPosition.java
+++ src/main/java/org/dominokit/domino/ui/notifications/TopCenterPosition.java
@@ -12,8 +12,9 @@ public class TopCenterPosition extends NotificationPosition {
     @Override
     protected void onBeforePosition(HTMLElement element) {
         element.style.setProperty("top", "20px");
-        element.style.setProperty("left", "0px");
         element.style.setProperty("right", "0px");
+        element.style.setProperty("left", "50%");
+        element.style.setProperty("transform", "translate(-50%)");
     }
 
     @Override
