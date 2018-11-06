8ac12ea1fe0bd1bbcd41b8c4f5e3f48d57da2ea9
Ahmad Bawaneh <akabme@gmail.com>
2018-11-06 10:50:36 +0200

    refactor : move fields grouping to its own interface

--- src/main/java/org/dominokit/domino/ui/forms/FormElement.java
+++ src/main/java/org/dominokit/domino/ui/forms/FormElement.java
@@ -6,11 +6,11 @@ import com.google.gwt.user.client.TakesValue;
 import org.dominokit.domino.ui.utils.*;
 
 public interface FormElement<T, V> extends HasName<T>, HasValue<T, V>, Switchable<T>,
-        HasHelperText<T>, HasLabel<T>, HasValidation<T>, HasAutoValidation<T>, IsRequired<T>, TakesValue<V>, IsEditor<TakesValueEditor<V>> {
+        HasHelperText<T>, HasLabel<T>, HasValidation<T>, HasAutoValidation<T>, IsRequired<T>, TakesValue<V>, IsEditor<TakesValueEditor<V>>, HasGrouping<T> {
 
     boolean isEmpty();
 
     T clear();
 
-    T groupBy(FieldsGrouping fieldsGrouping);
+
 }
