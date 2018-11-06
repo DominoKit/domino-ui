a7813d6b5f8d5edd4eff37ec8eeb14593d592473
Ahmad Bawaneh <akabme@gmail.com>
2018-11-06 14:50:38 +0200

    ignore public methods from super classes for editors

--- src/main/java/org/dominokit/domino/ui/utils/Switchable.java
+++ src/main/java/org/dominokit/domino/ui/utils/Switchable.java
@@ -1,9 +1,15 @@
 package org.dominokit.domino.ui.utils;
 
+import com.google.gwt.editor.client.Editor;
+
 public interface Switchable<T> {
+
+    @Editor.Ignore
     T enable();
 
+    @Editor.Ignore
     T disable();
 
+    @Editor.Ignore
     boolean isEnabled();
 }
