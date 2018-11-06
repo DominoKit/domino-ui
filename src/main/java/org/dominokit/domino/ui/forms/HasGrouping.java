31ec7a46e3c944d77886258e6bf400c27f370171
Ahmad Bawaneh <akabme@gmail.com>
2018-11-06 14:50:38 +0200

    ignore public methods from super classes for editors

--- src/main/java/org/dominokit/domino/ui/forms/HasGrouping.java
+++ src/main/java/org/dominokit/domino/ui/forms/HasGrouping.java
@@ -1,14 +1,19 @@
 package org.dominokit.domino.ui.forms;
 
+import com.google.gwt.editor.client.Editor;
 import org.dominokit.domino.ui.utils.HasAutoValidation;
 import org.dominokit.domino.ui.utils.HasValidation;
 import org.dominokit.domino.ui.utils.IsRequired;
 import org.dominokit.domino.ui.utils.Switchable;
 
 public interface HasGrouping<T> extends Switchable<T>, IsRequired<T>, HasValidation<T>, HasAutoValidation<T> {
+
+    @Editor.Ignore
     T groupBy(FieldsGrouping fieldsGrouping);
 
+    @Editor.Ignore
     boolean isEmpty();
 
+    @Editor.Ignore
     T clear();
 }
