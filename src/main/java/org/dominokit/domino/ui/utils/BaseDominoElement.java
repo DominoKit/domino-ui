d9dcb5c0a3459949da2795b7d8bbfd0676f4936d
Ahmad Bawaneh <akabme@gmail.com>
2018-11-06 14:50:38 +0200

    ignore public methods from super classes for editors

--- src/main/java/org/dominokit/domino/ui/utils/BaseDominoElement.java
+++ src/main/java/org/dominokit/domino/ui/utils/BaseDominoElement.java
@@ -42,40 +42,47 @@ public abstract class BaseDominoElement<E extends HTMLElement, T extends IsEleme
         return element;
     }
 
+    @Editor.Ignore
     public String getId() {
         return asElement().id;
     }
 
     @Override
+    @Editor.Ignore
     public T collapse() {
         collapsible.collapse();
         return element;
     }
 
     @Override
+    @Editor.Ignore
     public T expand() {
         collapsible.expand();
         return element;
     }
 
     @Override
+    @Editor.Ignore
     public T toggleDisplay() {
         collapsible.toggleDisplay();
         return element;
     }
 
     @Override
+    @Editor.Ignore
     public T toggleDisplay(boolean state) {
         collapsible.toggleDisplay(state);
         return element;
     }
 
+    @Editor.Ignore
     public T clearElement() {
         ElementUtil.clear(asElement());
         return element;
     }
 
     @Override
+    @Editor.Ignore
     public boolean isCollapsed() {
         if (isNull(collapsible)) {
             return false;
@@ -83,6 +90,7 @@ public abstract class BaseDominoElement<E extends HTMLElement, T extends IsEleme
         return collapsible.isCollapsed();
     }
 
+    @Editor.Ignore
     public T onAttached(ObserverCallback observerCallback) {
         if (!isAttached()) {
             ElementUtil.onAttach(element, observerCallback);
@@ -90,109 +98,131 @@ public abstract class BaseDominoElement<E extends HTMLElement, T extends IsEleme
         return element;
     }
 
+    @Editor.Ignore
     public T onDetached(ObserverCallback observerCallback) {
         ElementUtil.onDetach(element, observerCallback);
         return element;
     }
 
+    @Editor.Ignore
     public boolean isAttached() {
         return nonNull(DomGlobal.document.body.querySelector("[domino-uuid='" + uuid + "']"));
     }
 
+    @Editor.Ignore
     public Style<E, T> style() {
         return style;
     }
 
+    @Editor.Ignore
     public HtmlComponentBuilder<E, T> builder() {
         return ElementUtil.componentBuilder(element);
     }
 
     @Override
+    @Editor.Ignore
     public T appendChild(Node node) {
         element.asElement().appendChild(node);
         return element;
     }
 
     @Override
+    @Editor.Ignore
     public T appendChild(IsElement isElement) {
         element.asElement().appendChild(isElement.asElement());
         return element;
     }
 
+    @Editor.Ignore
     public T addClickListener(EventListener listener) {
         getClickableElement().addEventListener(EventType.click.getName(), listener);
         return element;
     }
 
+    @Editor.Ignore
     public T addEventListener(String type, EventListener listener) {
         asElement().addEventListener(type, listener);
         return element;
     }
 
+    @Editor.Ignore
     public T addEventListener(EventType type, EventListener listener) {
         asElement().addEventListener(type.getName(), listener);
         return element;
     }
 
+    @Editor.Ignore
     public T removeEventListener(EventType type, EventListener listener) {
         asElement().removeEventListener(type.getName(), listener);
         return element;
     }
 
+    @Editor.Ignore
     public T removeEventListener(String type, EventListener listener) {
         asElement().removeEventListener(type, listener);
         return element;
     }
 
+    @Editor.Ignore
     public T insertBefore(Node newNode, Node oldNode) {
         asElement().insertBefore(newNode, oldNode);
         return (T) this;
     }
 
+    @Editor.Ignore
     public T insertBefore(Node newNode, BaseDominoElement oldNode) {
         asElement().insertBefore(newNode, oldNode.asElement());
         return element;
     }
 
+    @Editor.Ignore
     public T insertBefore(BaseDominoElement newNode, BaseDominoElement oldNode) {
         asElement().insertBefore(newNode.asElement(), oldNode.asElement());
         return element;
     }
 
+    @Editor.Ignore
     public T insertBefore(BaseDominoElement newNode, Node oldNode) {
         asElement().insertBefore(newNode.asElement(), oldNode);
         return element;
     }
 
+    @Editor.Ignore
     public T insertFirst(Node newNode) {
         asElement().insertBefore(newNode, asElement().firstChild);
         return element;
     }
 
+    @Editor.Ignore
     public T insertFirst(IsElement element) {
         return insertFirst(element.asElement());
     }
 
+    @Editor.Ignore
     public T insertFirst(BaseDominoElement newNode) {
         asElement().insertBefore(newNode.asElement(), asElement().firstChild);
         return element;
     }
 
+    @Editor.Ignore
     public T setAttribute(String name, String value) {
         asElement().setAttribute(name, value);
         return element;
     }
 
+    @Editor.Ignore
     public T setAttribute(String name, boolean value) {
         asElement().setAttribute(name, value);
         return element;
     }
 
+    @Editor.Ignore
     public T setAttribute(String name, double value) {
         asElement().setAttribute(name, value);
         return element;
     }
 
+    @Editor.Ignore
     public T setReadonly(boolean readonly) {
         if (readonly) {
             return setAttribute("readonly", "readonly");
@@ -201,80 +231,98 @@ public abstract class BaseDominoElement<E extends HTMLElement, T extends IsEleme
         }
     }
 
+    @Editor.Ignore
     public String getAttribute(String name) {
         return asElement().getAttribute(name);
     }
 
+    @Editor.Ignore
     public T removeAttribute(String name) {
         asElement().removeAttribute(name);
         return element;
     }
 
+    @Editor.Ignore
     public boolean hasAttribute(String name) {
         return asElement().hasAttribute(name);
     }
 
+    @Editor.Ignore
     public boolean contains(DominoElement<? extends HTMLElement> node) {
         return contains(node.asElement());
     }
 
+    @Editor.Ignore
     public boolean contains(Node node) {
         return asElement().contains(node);
     }
 
+    @Editor.Ignore
     public T setTextContent(String text) {
         asElement().textContent = text;
         return element;
     }
 
+    @Editor.Ignore
     public T remove() {
         asElement().remove();
         return element;
     }
 
+    @Editor.Ignore
     public T removeChild(Node node) {
         asElement().removeChild(node);
         return element;
     }
 
+    @Editor.Ignore
     public T removeChild(IsElement<HTMLElement> elementToRemove) {
         removeChild(elementToRemove.asElement());
         return element;
     }
 
+    @Editor.Ignore
     public NodeList<Node> childNodes(Node node) {
         return asElement().childNodes;
     }
 
+    @Editor.Ignore
     public Node firstChild() {
         return asElement().firstChild;
     }
 
+    @Editor.Ignore
     public Node lastChild() {
         return asElement().lastChild;
     }
 
+    @Editor.Ignore
     public String getTextContent() {
         return asElement().textContent;
     }
 
+    @Editor.Ignore
     public T blur() {
         asElement().blur();
         return element;
     }
 
+    @Editor.Ignore
     public T setTooltip(String text) {
         return setTooltip(text, PopupPosition.TOP);
     }
 
+    @Editor.Ignore
     public T setTooltip(String text, PopupPosition position) {
         return setTooltip(TextNode.of(text), position);
     }
 
+    @Editor.Ignore
     public T setTooltip(Node node) {
         return setTooltip(node, PopupPosition.TOP);
     }
 
+    @Editor.Ignore
     public T setTooltip(Node node, PopupPosition position) {
         if (isNull(tooltip)) {
             tooltip = Tooltip.create(asElement(), node);
@@ -285,19 +333,23 @@ public abstract class BaseDominoElement<E extends HTMLElement, T extends IsEleme
         return element;
     }
 
+    @Editor.Ignore
     public HTMLElement getClickableElement() {
         return asElement();
     }
 
+    @Editor.Ignore
     public HTMLElement getCollapsibleElement() {
         return asElement();
     }
 
     @Override
+    @Editor.Ignore
     public HTMLElement getWavesElement() {
         return asElement();
     }
 
+    @Editor.Ignore
     public T hideOn(ScreenMedia screenMedia) {
         removeHideOn();
         this.hideOn = screenMedia;
@@ -306,6 +358,7 @@ public abstract class BaseDominoElement<E extends HTMLElement, T extends IsEleme
         return element;
     }
 
+    @Editor.Ignore
     public T removeHideOn() {
         if (nonNull(hideOn)) {
             style.remove("hide-on-" + hideOn.getStyle());
@@ -314,6 +367,7 @@ public abstract class BaseDominoElement<E extends HTMLElement, T extends IsEleme
         return element;
     }
 
+    @Editor.Ignore
     public T showOn(ScreenMedia screenMedia) {
         removeShowOn();
         this.showOn = screenMedia;
@@ -322,6 +376,7 @@ public abstract class BaseDominoElement<E extends HTMLElement, T extends IsEleme
         return element;
     }
 
+    @Editor.Ignore
     public T removeShowOn() {
         if (nonNull(showOn)) {
             style.remove("show-on-" + showOn.getStyle());
@@ -331,101 +386,122 @@ public abstract class BaseDominoElement<E extends HTMLElement, T extends IsEleme
     }
 
 
+    @Editor.Ignore
     public ClientRect getBoundingClientRect() {
         return element.asElement().getBoundingClientRect();
     }
 
+    @Editor.Ignore
     public T styler(StyleEditor<E, T> styleEditor) {
         styleEditor.applyStyles(style());
         return element;
     }
 
+    @Editor.Ignore
     public T addCss(String cssClass) {
         style().add(cssClass);
         return element;
     }
 
+    @Editor.Ignore
     public T addCss(String... cssClass) {
         style().add(cssClass);
         return element;
     }
 
+    @Editor.Ignore
     public T setWidth(String width) {
         style().setWidth(width);
         return element;
     }
 
+    @Editor.Ignore
     public T setHeight(String height) {
         style().setHeight(height);
         return element;
     }
 
+    @Editor.Ignore
     public boolean isEqualNode(Node node) {
         return asElement().isEqualNode(node);
     }
 
+    @Editor.Ignore
     public T withWaves() {
         WavesSupport.addFor(element.asElement());
         return element;
     }
 
+    @Editor.Ignore
     public T withWaves(WavesStyler wavesStyler) {
         wavesStyler.styleWaves(WavesSupport.addFor(element.asElement()));
         return element;
     }
 
+    @Editor.Ignore
     public T apply(ElementHandler<T> elementHandler) {
         elementHandler.handleElement(element);
         return element;
     }
 
 
+    @Editor.Ignore
     public T setContent(IsElement element){
         return setContent(element.asElement());
     }
 
+    @Editor.Ignore
     public T setContent(Node content){
         clearElement();
         appendChild(content);
         return element;
     }
 
+    @Editor.Ignore
     public int getElementsCount() {
         return new Double(asElement().childElementCount).intValue();
     }
 
+    @Editor.Ignore
     public boolean isEmptyElement() {
         return getElementsCount() == 0;
     }
 
+    @Editor.Ignore
     public double getChildElementCount() {
         return asElement().childElementCount;
     }
 
+    @Editor.Ignore
     public Node getFirstChild() {
         return asElement().firstChild;
     }
 
+    @Editor.Ignore
     public boolean hasChildNodes() {
         return asElement().hasChildNodes();
     }
 
+    @Editor.Ignore
     public String getDominoId() {
         return uuid;
     }
 
+    @Editor.Ignore
     public T disable(){
         setAttribute("disabled", "");
         style().add("disabled");
         return element;
     }
 
+    @Editor.Ignore
     public T enable(){
         removeAttribute("disabled");
         style().remove("disabled");
         return element;
     }
 
+    @Editor.Ignore
     public void setDisabled(boolean disabled){
         if(disabled){
             disable();
--- src/main/java/org/dominokit/domino/ui/utils/HasValidation.java
+++ src/main/java/org/dominokit/domino/ui/utils/HasValidation.java
@@ -1,19 +1,26 @@
 package org.dominokit.domino.ui.utils;
 
+import com.google.gwt.editor.client.Editor;
 import org.dominokit.domino.ui.forms.validations.ValidationResult;
 
 public interface HasValidation<T> {
 
+    @Editor.Ignore
     ValidationResult validate();
 
+    @Editor.Ignore
     T addValidator(Validator validator);
 
+    @Editor.Ignore
     T removeValidator(Validator validator);
 
+    @Editor.Ignore
     boolean hasValidator(Validator validator);
 
+    @Editor.Ignore
     T invalidate(String errorMessage);
 
+    @Editor.Ignore
     T clearInvalid();
 
     @FunctionalInterface
