97aa8ebc5b02dcf2002c2c83b39f1e256ad278a7
Ahmad Bawaneh <akabme@gmail.com>
2018-11-15 14:44:48 +0200

    replacing number format

--- src/main/java/org/dominokit/domino/ui/utils/Intl.java
+++ src/main/java/org/dominokit/domino/ui/utils/Intl.java
@@ -1,4 +1,9 @@
 package org.dominokit.domino.ui.utils;
 
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsType;
+
+@JsType(isNative = true, namespace = JsPackage.GLOBAL)
 public class Intl {
+
 }
