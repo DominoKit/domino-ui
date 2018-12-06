50157280c05d07016aa12d2de6a6d7c663973227
Ahmad Bawaneh <akabme@gmail.com>
2018-12-06 10:00:26 +0200

    https://github.com/DominoKit/domino-ui/issues/161

---
+++ src/main/java/org/dominokit/domino/ui/breadcrumbs/BreadcrumbStyles.java
@@ -0,0 +1,7 @@
+package org.dominokit.domino.ui.breadcrumbs;
+
+public class BreadcrumbStyles {
+
+    public static final String BREADCRUMB = "breadcrumb";
+    public static final String ACTIVE = "active";
+}
