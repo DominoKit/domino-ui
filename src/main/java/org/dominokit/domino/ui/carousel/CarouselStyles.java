cdf813ca939158cc3834ca87285853891ce5d2ac
rjeeb <rafat.albarouki@gmail.com>
2018-12-14 13:50:43 +0200

    create styles for cards and carousel

---
+++ src/main/java/org/dominokit/domino/ui/carousel/CarouselStyles.java
@@ -0,0 +1,16 @@
+package org.dominokit.domino.ui.carousel;
+
+public class CarouselStyles {
+    public static final String CAROUSEL_INDICATORS = "carousel-indicators";
+    public static final String CAROUSEL_INNER = "carousel-inner";
+    public static final String CAROUSEL_CONTROL = "carousel-control";
+    public static final String CAROUSEL_CAPTION = "carousel-caption";
+    public static final String CAROUSEL = "carousel";
+    public static final String SLIDE = "slide";
+    public static final String ACTIVE = "active";
+    public static final String LEFT = "left";
+    public static final String RIGHT = "right";
+    public static final String NEXT = "next";
+    public static final String PREV = "prev";
+    public static final String ITEM = "item";
+}
