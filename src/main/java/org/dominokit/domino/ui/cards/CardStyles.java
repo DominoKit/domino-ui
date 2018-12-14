253a651439328ce3265677ad5d59bf55084b0d90
rjeeb <rafat.albarouki@gmail.com>
2018-12-14 13:50:43 +0200

    create styles for cards and carousel

---
+++ src/main/java/org/dominokit/domino/ui/cards/CardStyles.java
@@ -0,0 +1,10 @@
+package org.dominokit.domino.ui.cards;
+
+public class CardStyles {
+
+    public static final String CARD = "card";
+    public static final String HEADER = "header";
+    public static final String HEADER_ACTIONS = "header-actions";
+    public static final String BODY = "body";
+    public static final String ACTION_ICON = "action-icon";
+}
