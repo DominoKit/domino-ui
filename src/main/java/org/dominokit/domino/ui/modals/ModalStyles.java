1be29616e8c3227642ed0ecfe9850f067c4d28ce
Ahmad Bawaneh <akabme@gmail.com>
2019-01-07 00:21:28 +0200

    modals and message dialogs https://github.com/DominoKit/domino-ui/issues/161

---
+++ src/main/java/org/dominokit/domino/ui/modals/ModalStyles.java
@@ -0,0 +1,19 @@
+package org.dominokit.domino.ui.modals;
+
+public class ModalStyles {
+    public static final String MODAL_OPEN = "modal-open";
+
+    public static final String MODAL_LG = "modal-lg";
+    public static final String MODAL_ALERT = "modal-alert";
+    public static final String MODAL_SM = "modal-sm";
+
+    public static final String BOTTOM_SHEET = "bottom-sheet";
+    public static final String TOP_SHEET = "top-sheet";
+    public static final String LEFT_SHEET = "left-sheet";
+    public static final String RIGHT_SHEET = "right-sheet";
+
+    public static final String MODAL_BACKDROP = "modal-backdrop";
+    public static final String FADE = "fade";
+    public static final String IN = "in";
+    public static final String PICKER_MODAL = "picker-modal";
+}
