91f431ca7bd3d96250dea0bef7da9ebe3d8229e6
vegegoku <akabme@gmail.com>
2019-03-01 16:45:12 +0200

    fix for https://github.com/DominoKit/domino-ui/issues/186

--- src/main/java/org/dominokit/domino/ui/grid/flex/FlexAlign.java
+++ src/main/java/org/dominokit/domino/ui/grid/flex/FlexAlign.java
@@ -3,11 +3,11 @@ package org.dominokit.domino.ui.grid.flex;
 import org.dominokit.domino.ui.style.IsCssClass;
 
 public enum FlexAlign implements IsCssClass {
-    START("flex-align-start"),
-    END("flex-align-end"),
-    CENTER("flex-align-center"),
-    STRETCH("flex-align-stretch"),
-    BASE_LINE("flex-align-baseline");
+    START("flex-start"),
+    END("flex-end"),
+    CENTER("center"),
+    STRETCH("stretch"),
+    BASE_LINE("baseline");
 
     private String style;
 
