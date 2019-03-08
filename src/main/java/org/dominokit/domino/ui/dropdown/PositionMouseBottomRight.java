cf9d127b2656ea4bc98e6f8d44ef58618b8f9233
vegegoku <akabme@gmail.com>
2019-03-08 14:38:55 +0200

    add drop down menu position based on mouse event to pick the location

--- src/main/java/org/dominokit/domino/ui/dropdown/PositionMouseBottomRight.java
+++ src/main/java/org/dominokit/domino/ui/dropdown/PositionMouseBottomRight.java
@@ -1,29 +1,25 @@
 package org.dominokit.domino.ui.dropdown;
 
 import elemental2.dom.ClientRect;
-import elemental2.dom.DomGlobal;
 import elemental2.dom.HTMLElement;
 import elemental2.dom.MouseEvent;
 
-import static elemental2.dom.DomGlobal.window;
 import static org.dominokit.domino.ui.style.Unit.px;
 
-public class DropDownPositionMouseBottomLeft implements DropDownPosition {
+public class PositionMouseBottomRight implements DropDownPosition {
 
     private final MouseEvent mouseEvent;
 
-    public DropDownPositionMouseBottomLeft(MouseEvent mouseEvent) {
+    public PositionMouseBottomRight(MouseEvent mouseEvent) {
         this.mouseEvent = mouseEvent;
     }
 
     @Override
     public void position(HTMLElement actionsMenu, HTMLElement target) {
 
-        ClientRect targetRect = target.getBoundingClientRect();
         ClientRect actionsRect = actionsMenu.getBoundingClientRect();
 
-        DomGlobal.console.info(mouseEvent.clientY + " : "+(mouseEvent.clientY - actionsRect.width));
         actionsMenu.style.setProperty("top", px.of(mouseEvent.clientY));
-        actionsMenu.style.setProperty("left", px.of(mouseEvent.clientY - actionsRect.width));
+        actionsMenu.style.setProperty("left", px.of(mouseEvent.clientX));
     }
 }
