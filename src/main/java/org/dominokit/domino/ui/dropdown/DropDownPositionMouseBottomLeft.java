f97c3cf93be2502880ad98f951f7696a88802822
vegegoku <akabme@gmail.com>
2019-03-08 13:30:15 +0200

    add mouse position for dropdown

--- src/main/java/org/dominokit/domino/ui/dropdown/DropDownPositionMouseBottomLeft.java
+++ src/main/java/org/dominokit/domino/ui/dropdown/DropDownPositionMouseBottomLeft.java
@@ -2,16 +2,24 @@ package org.dominokit.domino.ui.dropdown;
 
 import elemental2.dom.ClientRect;
 import elemental2.dom.HTMLElement;
+import elemental2.dom.MouseEvent;
 
 import static elemental2.dom.DomGlobal.window;
 import static org.dominokit.domino.ui.style.Unit.px;
 
-public class DropDownPositionBottomLeft implements DropDownPosition {
+public class DropDownPositionMouseBottomLeft implements DropDownPosition {
+
+    private final MouseEvent mouseEvent;
+
+    public DropDownPositionMouseBottomLeft(MouseEvent mouseEvent) {
+        this.mouseEvent = mouseEvent;
+    }
+
     @Override
     public void position(HTMLElement actionsMenu, HTMLElement target) {
         ClientRect targetRect = target.getBoundingClientRect();
         ClientRect actionsRect = actionsMenu.getBoundingClientRect();
-        actionsMenu.style.setProperty("top", px.of((targetRect.top + window.pageYOffset) + targetRect.height));
-        actionsMenu.style.setProperty("left", px.of(targetRect.left + window.pageXOffset - actionsRect.width));
+        actionsMenu.style.setProperty("top", px.of(mouseEvent.clientY));
+        actionsMenu.style.setProperty("left", px.of(mouseEvent.clientY - actionsRect.width));
     }
 }
