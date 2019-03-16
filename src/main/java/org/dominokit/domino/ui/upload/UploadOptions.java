94efecc6e55c343bf08632de474cde4c2098466c
vegegoku <akabme@gmail.com>
2019-03-17 00:18:55 +0200

    add option to define file upload success status codes

--- src/main/java/org/dominokit/domino/ui/upload/UploadOptions.java
+++ src/main/java/org/dominokit/domino/ui/upload/UploadOptions.java
@@ -1,15 +1,20 @@
 package org.dominokit.domino.ui.upload;
 
+import java.util.List;
+import java.util.function.Supplier;
+
 public class UploadOptions {
     private String url;
     private double maxFileSize;
+    private Supplier<List<Integer>> successCodesProvider;
 
-    public UploadOptions(String url, double maxFileSize) {
+    public UploadOptions(String url, double maxFileSize, Supplier<List<Integer>> successCodesProvider) {
         this.url = url;
         this.maxFileSize = maxFileSize;
+        this.successCodesProvider = successCodesProvider;
     }
 
-    void setUrl(String url){
+    void setUrl(String url) {
         this.url = url;
     }
 
@@ -19,5 +24,9 @@ public class UploadOptions {
 
     public double getMaxFileSize() {
         return maxFileSize;
+    }
+
+    public Supplier<List<Integer>> getSuccessCodesProvider() {
+        return successCodesProvider;
     }
 }
