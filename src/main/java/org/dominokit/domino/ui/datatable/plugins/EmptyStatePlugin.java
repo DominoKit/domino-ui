8dc663a74674db99ecd1cc651e341b4fd0f665f9
vegegoku <akabme@gmail.com>
2019-04-03 22:06:53 +0300

    fix for https://github.com/DominoKit/domino-ui/issues/141, add RowClickPlugin

---
+++ src/main/java/org/dominokit/domino/ui/datatable/plugins/EmptyStatePlugin.java
@@ -0,0 +1,36 @@
+package org.dominokit.domino.ui.datatable.plugins;
+
+import org.dominokit.domino.ui.datatable.DataTable;
+import org.dominokit.domino.ui.datatable.events.TableDataUpdatedEvent;
+import org.dominokit.domino.ui.icons.BaseIcon;
+import org.dominokit.domino.ui.layout.EmptyState;
+import org.dominokit.domino.ui.style.Color;
+
+public class EmptyStatePlugin<T> implements DataTablePlugin<T> {
+
+    private EmptyState emptyState;
+
+    public EmptyStatePlugin(BaseIcon<?> emptyStateIcon, String title) {
+        emptyState = EmptyState.create(emptyStateIcon)
+                .setTitle(title)
+                .setIconColor(Color.GREY)
+                .setTitleColor(Color.GREY);
+    }
+
+    @Override
+    public void onAfterAddTable(DataTable dataTable) {
+        dataTable.addTableEventListner(TableDataUpdatedEvent.DATA_UPDATED, event -> {
+            TableDataUpdatedEvent tableDataUpdatedEvent = (TableDataUpdatedEvent) event;
+            if (tableDataUpdatedEvent.getTotalCount() == 0) {
+                emptyState.show();
+            } else {
+                emptyState.hide();
+            }
+        });
+        dataTable.asElement().appendChild(emptyState.asElement());
+    }
+
+    public EmptyState getEmptyState() {
+        return emptyState;
+    }
+}
