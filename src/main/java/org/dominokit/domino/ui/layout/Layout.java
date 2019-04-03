5e419d3b7ed0f266934c197918b2f70e22e32575
vegegoku <akabme@gmail.com>
2019-04-03 19:20:25 +0300

    fix for making auto an fix footer for small screens optional

--- src/main/java/org/dominokit/domino/ui/layout/Layout.java
+++ src/main/java/org/dominokit/domino/ui/layout/Layout.java
@@ -43,7 +43,9 @@ public class Layout extends BaseDominoElement<HTMLDivElement, Layout> {
     private boolean navigationBarExpanded = false;
     private boolean overlayVisible = false;
     private boolean leftPanelDisabled = false;
+    private boolean footerVisible = false;
     private boolean fixedLeftPanel;
+    private boolean fixedFooter = false;
     private LeftPanelSize leftPanelSize = LeftPanelSize.DEFAULT;
 
     private LayoutHandler onShowHandler = layout -> {
@@ -74,7 +76,25 @@ public class Layout extends BaseDominoElement<HTMLDivElement, Layout> {
                 .style()
                 .add(leftPanelSize.getSize());
 
-        MediaQuery.addOnSmallAndDownListener(this::unfixFooter);
+        MediaQuery.addOnSmallAndDownListener(() -> {
+            if(footer.isAutoUnFixForSmallScreens() && footer.isFixed()) {
+                fixedFooter = true;
+                unfixFooter();
+            }
+        });
+
+        MediaQuery.addOnSmallAndDownListener(() -> {
+            if(footer.isAutoUnFixForSmallScreens() && isFooterVisible()) {
+                fixedFooter = true;
+                unfixFooter();
+            }
+        });
+
+        MediaQuery.addOnMediumAndUpListener(() -> {
+            if(footer.isAutoUnFixForSmallScreens() && nonNull(fixedFooter) && fixedFooter && isFooterVisible()) {
+                fixFooter();
+            }
+        });
 
         if (nonNull(onShowHandler)) {
             onShowHandler.handleLayout(this);
@@ -263,6 +283,10 @@ public class Layout extends BaseDominoElement<HTMLDivElement, Layout> {
         return this;
     }
 
+    public boolean isFooterVisible() {
+        return footerVisible;
+    }
+
     public DominoElement<HTMLElement> getRightPanel() {
         return DominoElement.of(section.rightSide);
     }
@@ -298,6 +322,7 @@ public class Layout extends BaseDominoElement<HTMLDivElement, Layout> {
 
     public Layout showFooter() {
         footer.show();
+        this.footerVisible = true;
         return this;
     }
 
@@ -373,7 +398,7 @@ public class Layout extends BaseDominoElement<HTMLDivElement, Layout> {
     }
 
     public Layout fixFooter() {
-        footer.asElement().classList.add("fixed");
+        footer.fixed();
         if (footer.isAttached()) {
             updateContentBottomPadding();
         } else {
@@ -388,7 +413,7 @@ public class Layout extends BaseDominoElement<HTMLDivElement, Layout> {
     }
 
     public Layout unfixFooter() {
-        footer.asElement().classList.remove("fixed");
+        footer.unfixed();
         ElementUtil.onAttach(footer.asElement(), mutationRecord -> Style.of(content.asElement()).removeProperty("padding-bottom"));
         return this;
     }
