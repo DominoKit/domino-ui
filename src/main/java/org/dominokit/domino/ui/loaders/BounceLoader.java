85be1994c6642ed28598a9ac28ef5e351d0c6932
vegegoku <akabme@gmail.com>
2019-05-23 00:12:40 +0300

    apply css minify

--- src/main/java/org/dominokit/domino/ui/loaders/BounceLoader.java
+++ src/main/java/org/dominokit/domino/ui/loaders/BounceLoader.java
@@ -10,6 +10,19 @@ import static org.jboss.gwt.elemento.core.Elements.div;
 
 public class BounceLoader extends BaseLoader<BounceLoader> implements IsElement<HTMLDivElement> {
 
+    /*
+    <div class="waitMe" style="background: rgba(255, 255, 255, 0.9);">
+    <div class="waitMe_content v-center">
+        <div class="waitMe_progress bounce">
+            <div data-element="progress1" class="waitMe_progress_elem1" style="background-color:#555"></div>
+            <div data-element="progress2" class="waitMe_progress_elem2" style="background-color:#555"></div>
+            <div data-element="progress3" class="waitMe_progress_elem3" style="background-color:#555"></div>
+        </div>
+        <div data-element="loadingText" class="waitMe_text" style="color:#555">Loading...</div>
+    </div>
+</div>
+     */
+
     private HTMLDivElement progress1 = div().css(WAIT_ME_PROGRESS_ELEM_1).style("background-color:#555;").asElement();
     private HTMLDivElement progress2 = div().css(WAIT_ME_PROGRESS_ELEM_2).style("background-color:#555;").asElement();
     private HTMLDivElement progress3 = div().css(WAIT_ME_PROGRESS_ELEM_3).style("background-color:#555;").asElement();
