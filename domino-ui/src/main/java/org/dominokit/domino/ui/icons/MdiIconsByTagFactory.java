a4ec12b727010c39120f88368dffe2c65b772e5f
vegegoku <akabme@gmail.com>
2019-05-29 23:55:17 +0300

    add missing mdi interface

---
+++ domino-ui/src/main/java/org/dominokit/domino/ui/icons/MdiIconsByTagFactory.java
@@ -0,0 +1,9 @@
+package org.dominokit.domino.ui.icons;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+public interface MdiIconsByTagFactory {
+
+    List<Supplier<MdiIcon>> icons();
+}
