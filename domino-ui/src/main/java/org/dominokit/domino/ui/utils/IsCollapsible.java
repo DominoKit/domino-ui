2a621adbab313947c554c10980b624522a8ade8e
vegegoku <akabme@gmail.com>
2019-06-03 11:59:43 +0300

    fix for https://github.com/DominoKit/domino-ui/issues/213

--- domino-ui/src/main/java/org/dominokit/domino/ui/utils/IsCollapsible.java
+++ domino-ui/src/main/java/org/dominokit/domino/ui/utils/IsCollapsible.java
@@ -1,23 +1,6 @@
 package org.dominokit.domino.ui.utils;
 
 public interface IsCollapsible<T> {
-    /**
-     * @return T
-     * @deprecated use {@link #hide()}
-     */
-    @Deprecated
-    default T collapse() {
-        return hide();
-    }
-
-    /**
-     * @return T
-     * @deprecated use {@link #show()}
-     */
-    @Deprecated
-    default T expand() {
-        return show();
-    }
 
     T show();
 
@@ -26,15 +9,6 @@ public interface IsCollapsible<T> {
     T toggleDisplay();
 
     T toggleDisplay(boolean state);
-
-    /**
-     * @return boolean
-     * @deprecated use {@link #isHidden()}
-     */
-    @Deprecated
-    default boolean isCollapsed() {
-        return isHidden();
-    }
 
     boolean isHidden();
 }
