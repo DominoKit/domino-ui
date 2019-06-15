c42dcb98a1eff4ed0195a13777c63ffd49caec06
vegegoku <akabme@gmail.com>
2019-06-15 12:06:29 +0300

    fix for https://github.com/DominoKit/domino-ui/issues/217

--- domino-ui/src/main/java/org/dominokit/domino/ui/public/polyfill/polyfill-ie11.js
+++ domino-ui/src/main/java/org/dominokit/domino/ui/public/polyfill/polyfill-ie11.js
@@ -14,3 +14,34 @@
         });
     });
 })([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
+
+/**
+ * polyfill for document.scrollingElement
+ * https://github.com/yangg/scrolling-element
+ */
+(function () {
+    if (document.scrollingElement) {
+        return
+    }
+    var element = null
+    function scrollingElement () {
+        if (element) {
+            return element
+        } else if (document.body.scrollTop) {
+            // speed up if scrollTop > 0
+            return (element = document.body)
+        }
+        var iframe = document.createElement('iframe')
+        iframe.style.height = '1px'
+        document.documentElement.appendChild(iframe)
+        var doc = iframe.contentWindow.document
+        doc.write('<!DOCTYPE html><div style="height:9999em">x</div>')
+        doc.close()
+        var isCompliant = doc.documentElement.scrollHeight > doc.body.scrollHeight
+        iframe.parentNode.removeChild(iframe)
+        return (element = isCompliant ? document.documentElement : document.body)
+    }
+    Object.defineProperty(document, 'scrollingElement', {
+        get: scrollingElement
+    })
+})()
