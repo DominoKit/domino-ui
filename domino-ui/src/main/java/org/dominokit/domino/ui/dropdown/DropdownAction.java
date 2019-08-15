73d6f68e9184aab573456fed2d5174494ae983c1
Mamta Bansal <mamta.bansal.mail@gmail.com>
2019-08-15 16:14:38 +0200

    Added getter for setting roles

--- domino-ui/src/main/java/org/dominokit/domino/ui/dropdown/DropdownAction.java
+++ domino-ui/src/main/java/org/dominokit/domino/ui/dropdown/DropdownAction.java
@@ -135,4 +135,8 @@ public class DropdownAction extends BaseDominoElement<HTMLLIElement, DropdownAct
     public interface FocusHandler {
         void onFocus(DropdownAction dropdownAction);
     }
+
+    public HTMLLIElement getLiElement() {
+        return this.liElement;
+    }
 }
