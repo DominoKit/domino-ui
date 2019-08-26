2b6aa9f3e6764d0536bce137489e6bd2d7e679e8
Ahmad Bawaneh <ahmad.bawaneh@progressoft.com>
2019-08-26 22:47:25 +0300

    fix for #276

---
+++ domino-ui/src/main/java/org/dominokit/domino/ui/icons/UrlIcon.java
@@ -0,0 +1,48 @@
+package org.dominokit.domino.ui.icons;
+
+import org.dominokit.domino.ui.utils.DominoElement;
+
+import static java.util.Objects.nonNull;
+import static org.jboss.gwt.elemento.core.Elements.img;
+
+public class UrlIcon extends BaseIcon<UrlIcon> {
+
+    private final String url;
+
+    private UrlIcon(String url, String name) {
+        this.url = url;
+        this.name = name;
+        this.icon = DominoElement.of(img(url).asElement());
+        init(this);
+    }
+
+    public static UrlIcon create(String url, String name) {
+        return new UrlIcon(url, name);
+    }
+
+    @Override
+    public UrlIcon copy() {
+        return new UrlIcon(url, name);
+    }
+
+    @Override
+    protected UrlIcon doToggle() {
+        if (nonNull(toggleName)) {
+            if (this.style.contains(originalName)) {
+                this.style.remove(originalName);
+                this.style.add(toggleName);
+            } else {
+                this.style.add(originalName);
+                this.style.remove(toggleName);
+            }
+        }
+        return this;
+    }
+
+    @Override
+    public UrlIcon changeTo(BaseIcon icon) {
+        style.remove(getName());
+        style.add(icon.getName());
+        return null;
+    }
+}
