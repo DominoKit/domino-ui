8e243cb54abdff3a4d1ec302085e2c0a73005953
rjeeb <rafat.albarouki@gmail.com>
2019-08-26 15:36:01 +0300

    add method to set the background of the drop down menu, fix highligting in suggest box

--- domino-ui/src/main/java/org/dominokit/domino/ui/forms/SuggestItem.java
+++ domino-ui/src/main/java/org/dominokit/domino/ui/forms/SuggestItem.java
@@ -1,35 +1,20 @@
 package org.dominokit.domino.ui.forms;
 
-import elemental2.core.JsRegExp;
-import elemental2.core.JsString;
-import elemental2.dom.HTMLDivElement;
-import elemental2.dom.HTMLElement;
+import org.dominokit.domino.ui.dropdown.DropdownAction;
 import org.dominokit.domino.ui.icons.BaseIcon;
-import org.dominokit.domino.ui.icons.Icon;
 import org.dominokit.domino.ui.icons.Icons;
-import org.dominokit.domino.ui.utils.BaseDominoElement;
-import org.dominokit.domino.ui.utils.DominoElement;
+import org.dominokit.domino.ui.style.Color;
 
-import static org.jboss.gwt.elemento.core.Elements.div;
-import static org.jboss.gwt.elemento.core.Elements.span;
+public class SuggestItem {
 
-public class SuggestItem extends BaseDominoElement<HTMLDivElement, SuggestItem> {
-
-    private static final String IGNORE_CASE_FLAG = "ig";
-    private DominoElement<HTMLDivElement> element = DominoElement.of(div());
-    private DominoElement<HTMLElement> valueContainer = DominoElement.of(span());
-    private String value;
+    private DropdownAction element;
 
     public SuggestItem(String value) {
         this(value, Icons.ALL.text_fields());
     }
 
     public SuggestItem(String value, BaseIcon<?> icon) {
-        this.value = value;
-        element.appendChild(icon);
-        valueContainer.setTextContent(value);
-        element.appendChild(valueContainer);
-        init(this);
+        element = DropdownAction.create(value, icon, value);
     }
 
     public static SuggestItem create(String value) {
@@ -40,19 +25,15 @@ public class SuggestItem extends BaseDominoElement<HTMLDivElement, SuggestItem> 
         return new SuggestItem(value, icon);
     }
 
+    public void highlight(String value, Color highlightColor) {
+        element.highlight(value, highlightColor);
+    }
+
+    public DropdownAction asDropDownAction() {
+        return element;
+    }
+
     public String getValue() {
-        return value;
-    }
-
-    @Override
-    public HTMLDivElement asElement() {
-        return element.asElement();
-    }
-
-    public void highlight(String value) {
-        String innerHTML = valueContainer.getTextContent();
-        JsRegExp regExp = new JsRegExp(value, IGNORE_CASE_FLAG);
-        innerHTML = new JsString(innerHTML).replace(regExp, (valueToReplace, p1) -> "<b class=\"highlight\">" + valueToReplace + "</b>");
-        valueContainer.asElement().innerHTML = innerHTML;
+        return element.getValue();
     }
 }
