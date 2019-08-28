2a5362654b328e27d0da1ea2893c86cb614720f8
Ahmad Bawaneh <ahmad.bawaneh@progressoft.com>
2019-08-28 15:19:01 +0300

    fix wrong updated file

--- domino-ui/src/main/java/org/dominokit/domino/ui/tree/ToggleTarget.java
+++ domino-ui/src/main/java/org/dominokit/domino/ui/tree/ToggleTarget.java
@@ -1,4 +1,14 @@
 package org.dominokit.domino.ui.tree;
 
-public enum ClickExpandBehavior {
+public enum ToggleTarget {
+    ANY("tgl-any"), ICON("tgl-icon"), EXPAND_COLLAPSE_INDICATOR("tgl-expand");
+    private String style;
+
+    ToggleTarget(String style) {
+        this.style = style;
+    }
+
+    public String getStyle() {
+        return style;
+    }
 }
