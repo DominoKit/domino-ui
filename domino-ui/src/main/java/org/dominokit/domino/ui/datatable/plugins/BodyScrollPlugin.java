baad89aad71155abf9744f258c60a5f3faf2d2e2
Ahmad Bawaneh <ahmad.bawaneh@progressoft.com>
2019-12-02 16:44:40 +0200

    fix for #297

--- domino-ui/src/main/java/org/dominokit/domino/ui/datatable/plugins/BodyScrollPlugin.java
+++ domino-ui/src/main/java/org/dominokit/domino/ui/datatable/plugins/BodyScrollPlugin.java
@@ -1,5 +1,7 @@
 package org.dominokit.domino.ui.datatable.plugins;
 
+import elemental2.core.JsMath;
+import elemental2.dom.DomGlobal;
 import elemental2.dom.HTMLTableSectionElement;
 import org.dominokit.domino.ui.datatable.DataTable;
 import org.dominokit.domino.ui.datatable.events.BodyScrollEvent;
@@ -9,15 +11,17 @@ public class BodyScrollPlugin<T> implements DataTablePlugin<T> {
     public void onBodyAdded(DataTable<T> dataTable) {
         HTMLTableSectionElement tbody = dataTable.bodyElement().element();
         tbody.addEventListener("scroll", evt -> {
-            if (tbody.scrollTop == 0) {
+            double scrollTop = new Double(tbody.scrollTop).intValue();
+            if (scrollTop == 0) {
                 dataTable.fireTableEvent(new BodyScrollEvent(ScrollPosition.TOP));
             }
-            if (tbody.offsetHeight + tbody.scrollTop == tbody.scrollHeight) {
+            int offsetHeight = new Double(tbody.offsetHeight).intValue();
+            int scrollHeight = new Double(tbody.scrollHeight).intValue();
+
+            if (JsMath.abs(offsetHeight) + JsMath.abs(scrollTop) == new Double(scrollHeight).intValue()) {
                 dataTable.fireTableEvent(new BodyScrollEvent(ScrollPosition.BOTTOM));
             }
         });
-
-
     }
 
     public enum ScrollPosition{
