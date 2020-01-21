1b14fe3fc3028d8c034c4d42e632827771fee7e5
Ahmad Bawaneh <ahmad.bawaneh@progressoft.com>
2020-01-21 14:56:46 +0200

    add missing suggestion provider to suggestBox

--- domino-ui/src/main/java/org/dominokit/domino/ui/forms/SuggestBoxStore.java
+++ domino-ui/src/main/java/org/dominokit/domino/ui/forms/SuggestBoxStore.java
@@ -1,14 +1,22 @@
 package org.dominokit.domino.ui.forms;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public interface SuggestBoxStore<T> {
 
     void filter(String value, SuggestionsHandler<T> suggestionsHandler);
+
     void find(T searchValue, Consumer<SuggestItem<T>> handler);
-    default boolean filterItem(String searchValue, SuggestItem<T> suggestItem){
+
+    default boolean filterItem(String searchValue, SuggestItem<T> suggestItem) {
         return suggestItem.getDisplayValue().toLowerCase().contains(searchValue.toLowerCase());
+    }
+
+    default MissingSuggestProvider<T> getMessingSuggestionProvider() {
+        return missingValue -> Optional.empty();
     }
 
     @FunctionalInterface
@@ -17,7 +25,12 @@ public interface SuggestBoxStore<T> {
     }
 
     @FunctionalInterface
-    interface SuggestionFilter<T>{
+    interface SuggestionFilter<T> {
         boolean filter(String searchValue, SuggestItem<T> suggestItem);
+    }
+
+    @FunctionalInterface
+    interface MissingSuggestProvider<T>{
+        Optional<SuggestItem<T>> getMessingSuggestion(T missingValue);
     }
 }
