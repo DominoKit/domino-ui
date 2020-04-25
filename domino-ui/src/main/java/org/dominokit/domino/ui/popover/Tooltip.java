07b0090b37bceab86eb9ee5187469784329fd2e3
Ahmad bawaneh <akabme@gmail.com>
2020-04-25 15:06:55 +0300

    fix: #403 Allow complete removal of tooltip already attached to an element

--- domino-ui/src/main/java/org/dominokit/domino/ui/popover/Tooltip.java
+++ domino-ui/src/main/java/org/dominokit/domino/ui/popover/Tooltip.java
@@ -1,19 +1,19 @@
 package org.dominokit.domino.ui.popover;
 
-import elemental2.dom.DomGlobal;
-import elemental2.dom.HTMLDivElement;
-import elemental2.dom.HTMLElement;
-import elemental2.dom.Node;
+import elemental2.dom.*;
 import org.dominokit.domino.ui.utils.BaseDominoElement;
 import org.dominokit.domino.ui.utils.DominoElement;
+import org.dominokit.domino.ui.utils.ElementObserver;
 import org.dominokit.domino.ui.utils.ElementUtil;
 import org.jboss.elemento.EventType;
 import org.jboss.elemento.IsElement;
 
+import java.util.Optional;
+import java.util.function.Consumer;
+
 import static elemental2.dom.DomGlobal.document;
 import static org.dominokit.domino.ui.popover.PopupPosition.TOP;
 import static org.jboss.elemento.Elements.div;
-import static org.jboss.elemento.Elements.tr;
 
 public class Tooltip extends BaseDominoElement<HTMLDivElement, Tooltip> {
 
@@ -21,6 +21,10 @@ public class Tooltip extends BaseDominoElement<HTMLDivElement, Tooltip> {
     private DominoElement<HTMLDivElement> arrowElement = DominoElement.of(div().css("tooltip-arrow"));
     private DominoElement<HTMLDivElement> innerElement = DominoElement.of(div().css("tooltip-inner"));
     private PopupPosition popupPosition = TOP;
+    private EventListener showToolTipListener;
+    private Consumer<Tooltip> removeHandler;
+    private EventListener removeToolTipListener;
+    private Optional<ElementObserver> elementObserver = Optional.empty();
 
     public Tooltip(HTMLElement targetElement, String text) {
         this(targetElement, DomGlobal.document.createTextNode(text));
@@ -33,24 +37,38 @@ public class Tooltip extends BaseDominoElement<HTMLDivElement, Tooltip> {
 
         element.style().add(popupPosition.getDirectionClass());
 
-            targetElement.addEventListener(EventType.mouseenter.getName(), evt -> {
-                evt.stopPropagation();
-                document.body.appendChild(element.element());
-                element.style().remove("fade", "in");
-                element.style().add("fade", "in");
-                popupPosition.position(element.element(), targetElement);
-                position(popupPosition);
-                ElementUtil.onDetach(targetElement, mutationRecord -> remove());
-            });
+        showToolTipListener = evt -> {
+            evt.stopPropagation();
+            document.body.appendChild(element.element());
+            element.style().remove("fade", "in");
+            element.style().add("fade", "in");
+            popupPosition.position(element.element(), targetElement);
+            position(popupPosition);
+            elementObserver = ElementUtil.onDetach(targetElement, mutationRecord -> remove());
 
-        targetElement.addEventListener(EventType.mouseleave.getName(), evt1 -> element.remove());
+        };
+        removeToolTipListener = evt -> element.remove();
+        targetElement.addEventListener(EventType.mouseenter.getName(), showToolTipListener);
+
+        targetElement.addEventListener(EventType.mouseleave.getName(), removeToolTipListener);
         init(this);
+
+        removeHandler = tooltip -> {
+          targetElement.removeEventListener(EventType.mouseenter.getName(), showToolTipListener);
+          targetElement.removeEventListener(EventType.mouseleave.getName(), removeToolTipListener);
+            elementObserver.ifPresent(ElementObserver::remove);
+        };
     }
 
     @Override
     public Tooltip hide() {
         element.remove();
         return this;
+    }
+
+    public void detach(){
+        removeHandler.accept(this);
+        remove();
     }
 
     public static Tooltip create(HTMLElement target, String text) {
