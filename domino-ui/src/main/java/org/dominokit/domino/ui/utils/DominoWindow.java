50ad100f474a8d35932f73b8dac29e40749af111
Ahmad bawaneh <akabme@gmail.com>
2020-06-22 17:27:31 +0300

    fix DominoWindow does not define the name as window

--- domino-ui/src/main/java/org/dominokit/domino/ui/utils/DominoWindow.java
+++ domino-ui/src/main/java/org/dominokit/domino/ui/utils/DominoWindow.java
@@ -6,7 +6,7 @@ import elemental2.dom.Window;
 import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsType;
 
-@JsType(isNative = true, namespace = JsPackage.GLOBAL)
+@JsType(isNative = true, namespace = JsPackage.GLOBAL, name = "window")
 public class DominoWindow extends Window {
     public native CSSStyleDeclaration getComputedStyle(Element element);
 }
