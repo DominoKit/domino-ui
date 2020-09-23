100d93d665ee194844d10d5d5e50a5d654b638ac
Ahmad bawaneh <akabme@gmail.com>
2020-09-23 11:27:15 +0300

    fix #471 Introduce the ability to create new colors and color themes, add docs

--- domino-ui-shared/src/main/java/org/dominokit/domino/ui/ColorInfo.java
+++ domino-ui-shared/src/main/java/org/dominokit/domino/ui/ColorInfo.java
@@ -2,15 +2,31 @@ package org.dominokit.domino.ui;
 
 import java.lang.annotation.*;
 
+/**
+ * An annotation to setup the information for a specific color to generate its assets and classes using the {@link ColorsSet}
+ */
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.PACKAGE)
 @Documented
 @Repeatable(ColorsSet.class)
 public @interface ColorInfo {
 
+    /**
+     * The color name, avoid using names already used by domino-ui core or other ColorSets to avoid css classes clashes.
+     * @return
+     */
     String name();
+
+    /**
+     * The hex value for the base color, this will be used to generate different shades in case a shade is not specified in the {@link ColorShades}
+     * @return
+     */
     String hex();
 
+    /**
+     * Use to manually specify a specific shade for the base color and avoid the generated shade.
+     * @return
+     */
     ColorShades shades() default @ColorShades;
 
 }
