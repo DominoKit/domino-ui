3b694b5c474e76c971c213a34d2f1de2a89cda40
Ahmad bawaneh <akabme@gmail.com>
2020-09-23 11:27:15 +0300

    fix #471 Introduce the ability to create new colors and color themes, add docs

--- domino-ui-shared/src/main/java/org/dominokit/domino/ui/ColorShades.java
+++ domino-ui-shared/src/main/java/org/dominokit/domino/ui/ColorShades.java
@@ -2,6 +2,9 @@ package org.dominokit.domino.ui;
 
 import java.lang.annotation.*;
 
+/**
+ * Specify shades for a specific color to be used by {@link ColorInfo}
+ */
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.PACKAGE)
 @Documented
@@ -18,7 +21,16 @@ public @interface ColorShades {
     String hex_darken_3() default "";
     String hex_darken_4() default "";
 
+    /**
+     * default to RGB with alpha 0.1
+     * @return
+     */
     String rgba_1() default "";
+
+    /**
+     * default to RGB with alpha 0.5
+     * @return
+     */
     String rgba_2() default "";
 
 }
--- domino-ui-shared/src/main/java/org/dominokit/domino/ui/ColorsSet.java
+++ domino-ui-shared/src/main/java/org/dominokit/domino/ui/ColorsSet.java
@@ -2,12 +2,38 @@ package org.dominokit.domino.ui;
 
 import java.lang.annotation.*;
 
+/**
+ * Use this annotation on a package-info class to generate new colors and colors assets for use by applications using domino-ui
+ * the annotation will result in generating a Color with 10 different shades for each {@link ColorInfo} , a theme-xxx.css for each {@link ColorInfo},
+ * A xxx-color.css for each, and an optional html page to demo the generated styles.
+ *
+ */
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.PACKAGE)
 @Documented
 public @interface ColorsSet {
+    /**
+     * A prefix to be used for classes names and css files names.
+     * @return
+     */
     String name() default "App";
+
+    /**
+     * the target package to generate the CSS resources, this should point to the <b>public</b> root package.
+     * classes will be generated to the same package as the package-info class.
+     * @return
+     */
     String publicPackage() default "";
+
+    /**
+     * Use this to specify the info for each color in this this color set
+     * @return
+     */
     ColorInfo[] value();
+
+    /**
+     * Option to specify if a demo html page should be generated or not.
+     * @return
+     */
     boolean generateDemoPage() default true;
 }
