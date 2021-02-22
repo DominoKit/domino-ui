df9f66cb8bce3faeac5aed395c7129d2b38fb27e
rjeeb <rafat.albarouki@gmail.com>
2021-02-22 19:53:14 +0200

    preparing for release, adds javadocs for Alert and Badge

--- domino-ui/src/main/java/org/dominokit/domino/ui/alerts/AlertStyles.java
+++ domino-ui/src/main/java/org/dominokit/domino/ui/alerts/AlertStyles.java
@@ -1,13 +1,40 @@
 package org.dominokit.domino.ui.alerts;
 
+/**
+ * Default CSS classes for {@link Alert}
+ */
 public class AlertStyles {
 
+    /**
+     * CSS class for alert
+     */
     public static final String ALERT = "alert";
+    /**
+     * CSS class for alert with success severity
+     */
     public static final String SUCCESS = "alert-success";
+    /**
+     * CSS class for alert with info severity
+     */
     public static final String INFO = "alert-info";
+    /**
+     * CSS class for alert with warning severity
+     */
     public static final String WARNING = "alert-warning";
+    /**
+     * CSS class for alert with error severity
+     */
     public static final String ERROR = "alert-danger";
+    /**
+     * CSS class for the close button
+     */
     public static final String CLOSE = "close";
+    /**
+     * CSS class for setting the alert as dismissible
+     */
     public static final String ALERT_DISMISSIBLE = "alert-dismissible";
+    /**
+     * CSS class for links inside alert
+     */
     public static final String ALERT_LINK = "alert-link";
 }
