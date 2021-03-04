ecca5d2df073b623b4f6de59c03f59747507b6f3
Ahmad bawaneh <akabme@gmail.com>
2021-03-05 01:47:05 +0200

    add counter javadocs

--- domino-ui/src/main/java/org/dominokit/domino/ui/counter/Counter.java
+++ domino-ui/src/main/java/org/dominokit/domino/ui/counter/Counter.java
@@ -2,6 +2,24 @@ package org.dominokit.domino.ui.counter;
 
 import org.gwtproject.timer.client.Timer;
 
+/**
+ * A component for counting from min to max value with interval and increment
+ * <p>
+ *     The counter class will allow the user to make a counting trigger that starts from a starting value to an end value.
+ *     the increments are controlled with a time interval and the user will be notified through a handler everytime the count it changed
+ *
+ *     <p>e.g : count from 0 to 100 by 1 increment every 1 second and log the count to the console</p>
+ *     <pre>
+ *        Counter
+ *                 .countFrom(0)
+ *                 .countTo(100)
+ *                 .every(1000)
+ *                 .incrementBy(1)
+ *                 .onCount(count -> DomGlobal.console.info(count))
+ *                 .startCounting();
+ *     </pre>
+ * </p>
+ */
 public class Counter {
 
     private Timer timer;
@@ -22,6 +40,11 @@ public class Counter {
         initTimer();
     }
 
+    /**
+     * static factory to create a new counter instance with count start
+     * @param countFrom int
+     * @return {@link CanCountTo}
+     */
     public static CanCountTo countFrom(int countFrom){
         return new CounterBuilder(countFrom);
     }
@@ -48,6 +71,9 @@ public class Counter {
         }
     }
 
+    /**
+     * Starts the counter, if the counter is already counting it will reset and start counting from the beginning
+     */
     public void startCounting() {
         if (timer.isRunning())
             timer.cancel();
@@ -56,27 +82,69 @@ public class Counter {
         timer.scheduleRepeating(interval);
     }
 
+    /**
+     * An interface to define a handler to be called after each count
+     */
     @FunctionalInterface
     public interface CountHandler{
+        /**
+         *
+         * @param count int the current counter value
+         */
         void onCount(int count);
     }
 
+    /**
+     * An interface to provide the counter end value
+     */
     public interface CanCountTo{
+        /**
+         *
+         * @param countTo int , the counter end value
+         * @return {@link HasInterval}
+         */
         HasInterval countTo(int countTo);
     }
 
+    /**
+     * An interface to set the counter count interval
+     */
     public interface HasInterval{
+        /**
+         *
+         * @param interval int count interval in milliseconds
+         * @return
+         */
         HasIncrement every(int interval);
     }
 
+    /**
+     * An interface to set the counter increment
+     */
     public interface HasIncrement{
+        /**
+         *
+         * @param increment int, the counter increment value
+         * @return {@link HasCountHandler}
+         */
         HasCountHandler incrementBy(int increment);
     }
 
+    /**
+     * An interface to set the count handler
+     */
     public interface HasCountHandler{
+        /**
+         *
+         * @param handler {@link CountHandler}
+         * @return {@link Counter}
+         */
         Counter onCount(CountHandler handler);
     }
 
+    /**
+     * A fluent builder to force building a counter with required parameters
+     */
     public static class CounterBuilder implements CanCountTo, HasInterval, HasIncrement, HasCountHandler {
 
         private int countFrom;
@@ -88,24 +156,36 @@ public class Counter {
             this.countFrom = countFrom;
         }
 
+        /**
+         * {@inheritDoc}
+         */
         @Override
         public HasInterval countTo(int countTo) {
             this.countTo=countTo;
             return this;
         }
 
+        /**
+         * {@inheritDoc}
+         */
         @Override
         public HasIncrement every(int interval) {
             this.interval=interval;
             return this;
         }
 
+        /**
+         * {@inheritDoc}
+         */
         @Override
         public HasCountHandler incrementBy(int increment) {
             this.increment =increment;
             return this;
         }
 
+        /**
+         * {@inheritDoc}
+         */
         @Override
         public Counter onCount(CountHandler handler) {
             return new Counter(countFrom, countTo, interval, increment, handler);
