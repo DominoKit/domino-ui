0099a285c091b6c9543f65c952e0725a447241f4
rjeeb <rafat.albarouki@gmail.com>
2021-03-07 22:45:21 +0200

    add javadocs for block header

--- domino-ui/src/main/java/org/dominokit/domino/ui/header/BlockHeader.java
+++ domino-ui/src/main/java/org/dominokit/domino/ui/header/BlockHeader.java
@@ -10,13 +10,25 @@ import static java.util.Objects.isNull;
 import static java.util.Objects.nonNull;
 import static org.jboss.elemento.Elements.*;
 
+/**
+ * A component provides a header text with a description in a predefined style
+ * <p>
+ * Customize the component can be done by overwriting classes provided by {@link BlockHeaderStyles}
+ *
+ * <p>For example: </p>
+ * <pre>
+ *     BlockHeader.create("Header", "Description");
+ * </pre>
+ *
+ * @see BaseDominoElement
+ */
 public class BlockHeader extends BaseDominoElement<HTMLDivElement, BlockHeader> {
 
-    private HTMLDivElement element = div().css(BlockHeaderStyles.BLOCK_HEADER).element();
-    private HTMLHeadingElement headerElement = h(2).element();
+    private final HTMLDivElement element = div().css(BlockHeaderStyles.BLOCK_HEADER).element();
+    private final HTMLHeadingElement headerElement = h(2).element();
     private HTMLElement descriptionElement;
-    private Text headerText = TextNode.empty();
-    private Text descriptionText = TextNode.empty();
+    private final Text headerText = TextNode.empty();
+    private final Text descriptionText = TextNode.empty();
 
     private BlockHeader(String header) {
         this(header, null);
@@ -39,15 +51,31 @@ public class BlockHeader extends BaseDominoElement<HTMLDivElement, BlockHeader> 
         headerElement.appendChild(descriptionElement);
     }
 
+    /**
+     * Creates a header with a description
+     *
+     * @param header      the header text
+     * @param description the description text
+     * @return new instance
+     */
     public static BlockHeader create(String header, String description) {
         return new BlockHeader(header, description);
     }
 
+    /**
+     * Creates a header
+     *
+     * @param header the header text
+     * @return new instance
+     */
     public static BlockHeader create(String header) {
         return new BlockHeader(header);
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public BlockHeader appendChild(Node content) {
         if (isNull(descriptionElement))
             createDescriptionElement("");
@@ -55,10 +83,19 @@ public class BlockHeader extends BaseDominoElement<HTMLDivElement, BlockHeader> 
         return this;
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public BlockHeader appendChild(IsElement<?> content) {
         return appendChild(content.element());
     }
 
+    /**
+     * Change the positions of the header and description elements by setting the header under the description
+     *
+     * @return same instance
+     */
     public BlockHeader invert() {
         if (nonNull(descriptionElement)) {
             descriptionElement.remove();
@@ -68,23 +105,44 @@ public class BlockHeader extends BaseDominoElement<HTMLDivElement, BlockHeader> 
         return this;
     }
 
+    /**
+     * Adds text as a child to this component
+     *
+     * @param text the text
+     * @return same instance
+     */
     public BlockHeader appendText(String text) {
         return appendChild(DomGlobal.document.createTextNode(text));
     }
 
+    /**
+     * Sets the header text
+     *
+     * @param header the new text
+     * @return same instance
+     */
     public BlockHeader setHeader(String header) {
         headerText.textContent = header;
         return this;
     }
 
+    /**
+     * @return The header element
+     */
     public DominoElement<HTMLHeadingElement> getHeaderElement() {
         return DominoElement.of(headerElement);
     }
 
+    /**
+     * @return The description element
+     */
     public DominoElement<HTMLElement> getDescriptionElement() {
         return DominoElement.of(descriptionElement);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public HTMLDivElement element() {
         return element;
--- domino-ui/src/main/java/org/dominokit/domino/ui/header/BlockHeaderStyles.java
+++ domino-ui/src/main/java/org/dominokit/domino/ui/header/BlockHeaderStyles.java
@@ -1,5 +1,12 @@
 package org.dominokit.domino.ui.header;
 
+/**
+ * Default CSS classes for {@link BlockHeader}
+ */
 public class BlockHeaderStyles {
+
+    /**
+     * CSS class for a block header
+     */
     public static final String BLOCK_HEADER = "block-header";
 }
