616c1777e82df28019d2f4aa5a6989b15e2d7154
rjeeb <rafat.albarouki@gmail.com>
2021-03-07 20:37:58 +0200

    add javadocs for date picker

--- domino-ui/src/main/java/org/dominokit/domino/ui/datepicker/MonthContext.java
+++ domino-ui/src/main/java/org/dominokit/domino/ui/datepicker/MonthContext.java
@@ -2,6 +2,9 @@ package org.dominokit.domino.ui.datepicker;
 
 import elemental2.core.JsDate;
 
+/**
+ * A context which holds a month information
+ */
 public class MonthContext {
 
     private final int year;
@@ -14,6 +17,7 @@ public class MonthContext {
     public MonthContext(int year, int month) {
         this(year, month, new JsDate().getDate());
     }
+
     public MonthContext(int year, int month, int date) {
 
         JsDate jsDate = new JsDate(year, month, DatePickerUtil.getValidMonthDate(year, month, date));
@@ -27,40 +31,59 @@ public class MonthContext {
         this.firstDay = new JsDate(year, month, 1).getDay();
     }
 
+    /**
+     * @return A current month information
+     */
     public static MonthContext current() {
         JsDate date = new JsDate();
         return new MonthContext(date.getFullYear(), date.getMonth());
     }
 
-    public static MonthContext offset(int offset) {
-        JsDate date = new JsDate();
-        return new MonthContext(date.getFullYear(), date.getMonth());
-    }
-
+    /**
+     * @return The month before this one
+     */
     public MonthContext getMonthBefore() {
         return new MonthContext(month == 0 ? year - 1 : year, month == 0 ? 11 : (month - 1));
     }
 
+    /**
+     * @return The year of this month
+     */
     public int getYear() {
         return year;
     }
 
+    /**
+     * @return The month
+     */
     public int getMonth() {
         return month;
     }
 
+    /**
+     * @return The day of this month
+     */
     public int getDate() {
         return date;
     }
 
+    /**
+     * @return The current day
+     */
     public int getDay() {
         return day;
     }
 
+    /**
+     * @return The number of days
+     */
     public int getDays() {
         return days;
     }
 
+    /**
+     * @return The first day of the month
+     */
     public int getFirstDay() {
         return firstDay;
     }
