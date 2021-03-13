071409e7f2a76cf67859d801a42cf971220ea148
rjeeb <rafat.albarouki@gmail.com>
2021-03-13 21:44:49 +0200

    add javadocs for lists

--- domino-ui/src/main/java/org/dominokit/domino/ui/lists/ListItem.java
+++ domino-ui/src/main/java/org/dominokit/domino/ui/lists/ListItem.java
@@ -13,6 +13,13 @@ import java.util.Objects;
 
 import static java.util.Objects.nonNull;
 
+/**
+ * A component which represents an item inside a {@link ListGroup}
+ *
+ * @param <T> the type of the value object inside the item
+ * @see ListGroup
+ * @see BaseDominoElement
+ */
 public class ListItem<T> extends BaseDominoElement<HTMLLIElement, ListItem<T>> {
 
     private final ListGroup<T> listGroup;
@@ -58,68 +65,124 @@ public class ListItem<T> extends BaseDominoElement<HTMLLIElement, ListItem<T>> {
         }
     }
 
-    private ListItem<T> selectRange() {
+    private void selectRange() {
         listGroup.selectRange(this);
-        return this;
     }
 
-    private ListItem<T> deselectRange() {
+    private void deselectRange() {
         listGroup.deselectRange(this);
-        return this;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public HTMLLIElement element() {
         return element;
     }
 
+    /**
+     * @return The value
+     */
     public T getValue() {
         return value;
     }
 
+    /**
+     * @param value The new value of this item
+     */
     public void setValue(T value) {
         this.value = value;
     }
 
+    /**
+     * @return The root element
+     */
     public HTMLLIElement getElement() {
         return element;
     }
 
+    /**
+     * @param element The new root element
+     */
     public void setElement(HTMLLIElement element) {
         this.element = element;
     }
 
+    /**
+     * Checks if values are equal
+     *
+     * @param value the value to check
+     * @return true if values are equal, false otherwise
+     */
     public boolean valueEquals(T value) {
         return Objects.equals(this.value, value);
     }
 
+    /**
+     * Selects the item
+     *
+     * @return same instance
+     */
     public ListItem<T> select() {
         return select(false);
     }
 
+    /**
+     * Deselects the item
+     *
+     * @return same instance
+     */
     public ListItem<T> deselect() {
         return deselect(false);
     }
 
+    /**
+     * Selects the item with a boolean to indicate if this should inform handlers or not
+     *
+     * @param silent true for not informing the handlers associated, false otherwise
+     * @return same instance
+     */
     public ListItem<T> select(boolean silent) {
         this.listGroup.select(this, silent);
         return this;
     }
 
+    /**
+     * Deselects the item with a boolean to indicate if this should inform handlers or not
+     *
+     * @param silent true for not informing the handlers associated, false otherwise
+     * @return same instance
+     */
     public ListItem<T> deselect(boolean silent) {
         this.listGroup.deselect(this, silent);
         return this;
     }
 
+    /**
+     * @return True if the item is selected, false otherwise
+     */
     public boolean isSelected() {
         return selected;
     }
 
+    /**
+     * Sets a selection handler for which will be called when the selection is changed
+     *
+     * @param listener the {@link SelectionChangedListener} to set
+     * @return same instance
+     */
     public ListItem<T> onSelectionChange(SelectionChangedListener<T> listener) {
         this.selectionChangedListeners.add(listener);
         return this;
     }
 
+    /**
+     * Sets if this item can be selected
+     *
+     * @param selectable true to enable selecting the item, false otherwise
+     * @return same instance
+     */
     public ListItem<T> setSelectable(boolean selectable) {
         this.selectable = selectable;
         if (selectable) {
@@ -130,19 +193,49 @@ public class ListItem<T> extends BaseDominoElement<HTMLLIElement, ListItem<T>> {
         return this;
     }
 
+    /**
+     * @return True if this item will be selected when clicking on it
+     */
     public boolean isSelectOnClick() {
         return selectOnClick;
     }
 
+    /**
+     * Sets if this item should be selected when clicking on it
+     *
+     * @param selectOnClick true to select on click, false otherwise
+     * @return same instance
+     */
     public ListItem<T> setSelectOnClick(boolean selectOnClick) {
         this.selectOnClick = selectOnClick;
         return this;
     }
 
+    /**
+     * @return True if the item is enabled, false otherwise
+     */
     public boolean isEnabled() {
         return enabled;
     }
 
+    /**
+     * Sets if this item is enabled
+     *
+     * @param enabled true to enable the item, false otherwise
+     * @return same instance
+     */
+    public ListItem<T> setEnabled(boolean enabled) {
+        if (!enabled) {
+            deselect();
+            style().add("disabled");
+            this.enabled = false;
+        }
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean equals(Object other) {
         if (this == other) return true;
@@ -151,6 +244,9 @@ public class ListItem<T> extends BaseDominoElement<HTMLLIElement, ListItem<T>> {
         return value.equals(listItem.value);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int hashCode() {
         return Objects.hash(value);
@@ -173,12 +269,21 @@ public class ListItem<T> extends BaseDominoElement<HTMLLIElement, ListItem<T>> {
         }
     }
 
-    void fireSelectionHandlers(boolean selected){
+    void fireSelectionHandlers(boolean selected) {
         this.selectionChangedListeners.forEach(listener -> listener.onSelectionChanged(ListItem.this, selected));
     }
 
+    /**
+     * A handler that will be called when the item is selected or deselected
+     *
+     * @param <T> the type of the value object inside the item
+     */
     @FunctionalInterface
     public interface SelectionChangedListener<T> {
+        /**
+         * @param item     The item
+         * @param selected true if selected, false otherwise
+         */
         void onSelectionChanged(ListItem<? extends T> item, boolean selected);
     }
 }
--- domino-ui/src/main/java/org/dominokit/domino/ui/lists/ListStyles.java
+++ domino-ui/src/main/java/org/dominokit/domino/ui/lists/ListStyles.java
@@ -1,9 +1,27 @@
 package org.dominokit.domino.ui.lists;
 
+/**
+ * Default CSS classes for {@link ListGroup} and {@link ListItem}
+ */
 public class ListStyles {
+    /**
+     * CSS class for list group
+     */
     public static final String LIST_GROUP = "d-list";
+    /**
+     * CSS class for bordered list items
+     */
     public static final String BORDERED = "bordered";
+    /**
+     * CSS class for list item inside list group
+     */
     public static final String LIST_GROUP_ITEM = "d-list-item";
+    /**
+     * CSS class for selectable list items
+     */
     public static final String SELECTABLE = "d-selectable";
+    /**
+     * CSS class for selected list items
+     */
     public static final String SELECTED = "selected";
 }
