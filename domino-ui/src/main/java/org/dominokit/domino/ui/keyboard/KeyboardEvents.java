93f8243d2203a5dd21e7185f08703676e4fb5b63
rjeeb <rafat.albarouki@gmail.com>
2021-03-13 19:51:51 +0200

    add javadocs for keyboard events

--- domino-ui/src/main/java/org/dominokit/domino/ui/keyboard/KeyboardEvents.java
+++ domino-ui/src/main/java/org/dominokit/domino/ui/keyboard/KeyboardEvents.java
@@ -12,6 +12,11 @@ import java.util.Map;
 
 import static java.util.Objects.nonNull;
 
+/**
+ * A helper class allowing listening to keyboard events on an element
+ *
+ * @param <T> the element type
+ */
 public class KeyboardEvents<T extends Node> {
 
     public static final String ESCAPE = "escape";
@@ -24,8 +29,8 @@ public class KeyboardEvents<T extends Node> {
     public static final String TAB = "tab";
     public static final String BACKSPACE = "backspace";
 
-    private Map<String, HandlerContext> handlers = new HashMap<>();
-    private Map<String, HandlerContext> ctrlHandlers = new HashMap<>();
+    private final Map<String, HandlerContext> handlers = new HashMap<>();
+    private final Map<String, HandlerContext> ctrlHandlers = new HashMap<>();
     private KeyboardEventOptions defaultOptions = KeyboardEventOptions.create();
 
     public KeyboardEvents(T element) {
@@ -55,76 +60,193 @@ public class KeyboardEvents<T extends Node> {
         });
     }
 
-    public static <T extends Node> KeyboardEvents listenOn(T element) {
+    /**
+     * Static factory for creation keyboard event listener
+     *
+     * @param element the target element
+     * @param <T>     the type of the element
+     * @return new instance
+     */
+    public static <T extends Node> KeyboardEvents<T> listenOn(T element) {
         return new KeyboardEvents<>(element);
     }
 
-    public static KeyboardEvents listenOn(IsElement<?> element) {
+    /**
+     * Same as {@link KeyboardEvents#listenOn(Node)} but with wrapper {@link IsElement}
+     *
+     * @param element the target {@link IsElement}
+     * @param <T>     the type of the element
+     * @return new instance
+     */
+    public static <T extends HTMLElement> KeyboardEvents<T> listenOn(IsElement<T> element) {
         return new KeyboardEvents<>(element.element());
     }
 
+    /**
+     * On escape button pressed
+     *
+     * @param escapeHandler the {@link EventListener} to call
+     * @return same instance
+     */
     // ---------------- handlers ----------------
     public KeyboardEvents<T> onEscape(EventListener escapeHandler) {
         return onEscape(escapeHandler, defaultOptions());
     }
 
+    /**
+     * On escape button pressed with {@code options}
+     *
+     * @param escapeHandler the {@link EventListener} to call
+     * @param options       the {@link KeyboardEventOptions}
+     * @return same instance
+     */
     public KeyboardEvents<T> onEscape(EventListener escapeHandler, KeyboardEventOptions options) {
         return addHandler(ESCAPE, contextOf(escapeHandler, options));
     }
 
+    /**
+     * On arrow up or arrow down buttons pressed
+     *
+     * @param arrowDownHandler the {@link EventListener} to call
+     * @return same instance
+     */
     public KeyboardEvents<T> onArrowUpDown(EventListener arrowDownHandler) {
         return onArrowUp(arrowDownHandler).onArrowDown(arrowDownHandler);
     }
 
+    /**
+     * On arrow up or arrow down buttons pressed with options
+     *
+     * @param arrowDownHandler the {@link EventListener} to call
+     * @param options          the {@link KeyboardEventOptions}
+     * @return same instance
+     */
     public KeyboardEvents<T> onArrowUpDown(EventListener arrowDownHandler, KeyboardEventOptions options) {
         return onArrowUp(arrowDownHandler, options).onArrowDown(arrowDownHandler, options);
     }
 
-
+    /**
+     * On arrow down button pressed
+     *
+     * @param arrowDownHandler the {@link EventListener} to call
+     * @return same instance
+     */
     public KeyboardEvents<T> onArrowDown(EventListener arrowDownHandler) {
         return onArrowDown(arrowDownHandler, defaultOptions());
     }
 
+    /**
+     * On arrow down button pressed with options
+     *
+     * @param arrowDownHandler the {@link EventListener} to call
+     * @param options          the {@link KeyboardEventOptions}
+     * @return same instance
+     */
     public KeyboardEvents<T> onArrowDown(EventListener arrowDownHandler, KeyboardEventOptions options) {
         return addHandler(ARROWDOWN, contextOf(arrowDownHandler, options));
     }
 
+    /**
+     * On arrow up button pressed with options
+     *
+     * @param arrowUpHandler the {@link EventListener} to call
+     * @return same instance
+     */
     public KeyboardEvents<T> onArrowUp(EventListener arrowUpHandler) {
         return onArrowUp(arrowUpHandler, defaultOptions());
     }
 
+    /**
+     * On arrow up button pressed with options
+     *
+     * @param arrowUpHandler the {@link EventListener} to call
+     * @param options        the {@link KeyboardEventOptions}
+     * @return same instance
+     */
     public KeyboardEvents<T> onArrowUp(EventListener arrowUpHandler, KeyboardEventOptions options) {
         return addHandler(ARROWUP, contextOf(arrowUpHandler, options));
     }
 
+    /**
+     * On enter button pressed
+     *
+     * @param enterHandler the {@link EventListener} to call
+     * @return same instance
+     */
     public KeyboardEvents<T> onEnter(EventListener enterHandler) {
         return onEnter(enterHandler, defaultOptions());
     }
 
+    /**
+     * On enter button pressed with options
+     *
+     * @param enterHandler the {@link EventListener} to call
+     * @param options      the {@link KeyboardEventOptions}
+     * @return same instance
+     */
     public KeyboardEvents<T> onEnter(EventListener enterHandler, KeyboardEventOptions options) {
         return addHandler(ENTER, contextOf(enterHandler, options));
     }
 
+    /**
+     * On delete button pressed
+     *
+     * @param deleteHandler the {@link EventListener} to call
+     * @return same instance
+     */
     public KeyboardEvents<T> onDelete(EventListener deleteHandler) {
         return onDelete(deleteHandler, defaultOptions());
     }
 
+    /**
+     * On delete button pressed with options
+     *
+     * @param deleteHandler the {@link EventListener} to call
+     * @param options       the {@link KeyboardEventOptions}
+     * @return same instance
+     */
     public KeyboardEvents<T> onDelete(EventListener deleteHandler, KeyboardEventOptions options) {
         return addHandler(DELETE, contextOf(deleteHandler, options));
     }
 
+    /**
+     * On space button pressed
+     *
+     * @param spaceHandler the {@link EventListener} to call
+     * @return same instance
+     */
     public KeyboardEvents<T> onSpace(EventListener spaceHandler) {
         return onSpace(spaceHandler, defaultOptions());
     }
 
+    /**
+     * On space button pressed with options
+     *
+     * @param spaceHandler the {@link EventListener} to call
+     * @param options      the {@link KeyboardEventOptions}
+     * @return same instance
+     */
     public KeyboardEvents<T> onSpace(EventListener spaceHandler, KeyboardEventOptions options) {
         return addHandler(SPACE, contextOf(spaceHandler, options));
     }
 
+    /**
+     * On tab button pressed
+     *
+     * @param tabHandler the {@link EventListener} to call
+     * @return same instance
+     */
     public KeyboardEvents<T> onTab(EventListener tabHandler) {
         return onTab(tabHandler, defaultOptions());
     }
 
+    /**
+     * On tab button pressed with options
+     *
+     * @param tabHandler the {@link EventListener} to call
+     * @param options    the {@link KeyboardEventOptions}
+     * @return same instance
+     */
     public KeyboardEvents<T> onTab(EventListener tabHandler, KeyboardEventOptions options) {
         return addHandler(TAB, contextOf(tabHandler, options));
     }
@@ -135,14 +257,34 @@ public class KeyboardEvents<T extends Node> {
     }
 
     // ---------------- ctrl handlers ----------------
+
+    /**
+     * On ctrl + backspace buttons pressed
+     *
+     * @param ctrlBackspaceHandler the {@link EventListener} to call
+     * @return same instance
+     */
     public KeyboardEvents<T> onCtrlBackspace(EventListener ctrlBackspaceHandler) {
         return onCtrlBackspace(ctrlBackspaceHandler, defaultOptions());
     }
 
+    /**
+     * On ctrl + backspace buttons pressed with options
+     *
+     * @param ctrlBackspaceHandler the {@link EventListener} to call
+     * @param options              the {@link KeyboardEventOptions}
+     * @return same instance
+     */
     public KeyboardEvents<T> onCtrlBackspace(EventListener ctrlBackspaceHandler, KeyboardEventOptions options) {
         return addCtrlHandler(BACKSPACE, contextOf(ctrlBackspaceHandler, options));
     }
 
+    /**
+     * Sets the default {@link KeyboardEventOptions}
+     *
+     * @param defaultOptions the default {@link KeyboardEventOptions}
+     * @return same instance
+     */
     public KeyboardEvents<T> setDefaultOptions(KeyboardEventOptions defaultOptions) {
         this.defaultOptions = defaultOptions;
         return this;
@@ -161,19 +303,37 @@ public class KeyboardEvents<T extends Node> {
         return defaultOptions;
     }
 
+    /**
+     * Context to hold keyboard event options
+     */
     public static class KeyboardEventOptions {
         private boolean preventDefault = false;
         private boolean stopPropagation = false;
 
+        /**
+         * @return new instance
+         */
         public static KeyboardEventOptions create() {
             return new KeyboardEventOptions();
         }
 
+        /**
+         * Sets if prevent default behaviour is enabled or not
+         *
+         * @param preventDefault true to prevent default, false otherwise
+         * @return same instance
+         */
         public KeyboardEventOptions setPreventDefault(boolean preventDefault) {
             this.preventDefault = preventDefault;
             return this;
         }
 
+        /**
+         * Sets if stop event propagation is enabled or not
+         *
+         * @param stopPropagation true to stop propagation, false otherwise
+         * @return same instance
+         */
         public KeyboardEventOptions setStopPropagation(boolean stopPropagation) {
             this.stopPropagation = stopPropagation;
             return this;
@@ -181,8 +341,8 @@ public class KeyboardEvents<T extends Node> {
     }
 
     private static class HandlerContext {
-        private EventListener handler;
-        private KeyboardEventOptions options;
+        private final EventListener handler;
+        private final KeyboardEventOptions options;
 
         public HandlerContext(EventListener handler, KeyboardEventOptions options) {
             this.handler = handler;
