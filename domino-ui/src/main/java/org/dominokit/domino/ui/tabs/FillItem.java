f003c503ee40efaf066318e60075d89be3f38a02
Ahmad bawaneh <akabme@gmail.com>
2021-03-19 17:13:12 +0200

    add javadocs for tabs

--- domino-ui/src/main/java/org/dominokit/domino/ui/tabs/FillItem.java
+++ domino-ui/src/main/java/org/dominokit/domino/ui/tabs/FillItem.java
@@ -4,6 +4,9 @@ import elemental2.dom.HTMLDivElement;
 import org.dominokit.domino.ui.grid.flex.FlexItem;
 import org.dominokit.domino.ui.utils.BaseDominoElement;
 
+/**
+ * A component that adds a space between {@link VerticalTab}s in the {@link VerticalTabsPanel}
+ */
 public class FillItem extends BaseDominoElement<HTMLDivElement, FillItem> {
 
     private FlexItem element;
@@ -14,10 +17,17 @@ public class FillItem extends BaseDominoElement<HTMLDivElement, FillItem> {
         init(this);
     }
 
+    /**
+     *
+     * @return new instance
+     */
     public static FillItem create() {
         return new FillItem();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public HTMLDivElement element() {
         return element.element();
--- domino-ui/src/main/java/org/dominokit/domino/ui/tabs/TabsAlign.java
+++ domino-ui/src/main/java/org/dominokit/domino/ui/tabs/TabsAlign.java
@@ -1,14 +1,36 @@
 package org.dominokit.domino.ui.tabs;
 
+/**
+ * An enum to list possible values for tabs align
+ */
 public enum TabsAlign {
-    LEFT("tabs-left"), CENTER("tabs-center"), RIGHT("tabs-right");
+    /**
+     * Tabs headers will be aligned to the left of the tab panel
+     */
+    LEFT("tabs-left"),
+    /**
+     * Tabs headers will be aligned to the center of the tab panel
+     */
+    CENTER("tabs-center"),
+    /**
+     * Tabs headers will be aligned to the right of the tab panel
+     */
+    RIGHT("tabs-right");
 
     private String align;
 
+    /**
+     *
+     * @param align String css class name for the tab align
+     */
     TabsAlign(String align) {
         this.align = align;
     }
 
+    /**
+     *
+     * @return String css class name for the tab align
+     */
     public String getAlign() {
         return align;
     }
