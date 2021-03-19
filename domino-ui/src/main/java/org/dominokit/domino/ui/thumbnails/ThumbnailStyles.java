0f6a2e4d7a7e643a47edc1c0ba50ecf42b72a98d
rjeeb <rafat.albarouki@gmail.com>
2021-03-19 15:04:07 +0200

    add javadocs for thumbnail

---
+++ domino-ui/src/main/java/org/dominokit/domino/ui/thumbnails/ThumbnailStyles.java
@@ -0,0 +1,16 @@
+package org.dominokit.domino.ui.thumbnails;
+
+/**
+ * Default CSS styles for {@link Thumbnail}
+ */
+public class ThumbnailStyles {
+
+    /**
+     * CSS class for thumbnail
+     */
+    public static final String THUMBNAIL = "thumbnail";
+    /**
+     * CSS class for caption
+     */
+    public static final String CAPTION = "caption";
+}
