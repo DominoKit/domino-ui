DominoKit/domino-ui

commit 6ed0664db9c0bc2fb00409496816cbfab5e61bac

improve check box set label by removing local label variable, otherwise set label again does not have any ui effect. (#540)

domino-ui/src/main/java/org/dominokit/domino/ui/forms/CheckBox.java
@@ -36,7 +36,6 @@ public class CheckBox extends AbstractValueBox<CheckBox, HTMLInputElement, Boole
   private Color color;
   private String checkedReadonlyLabel = "Yes";
   private String unCheckedReadonlyLabel = "No";
-  private String label;
 
   /** Creates a checkbox without a label */
   public CheckBox() {
@@ -50,7 +49,6 @@ public class CheckBox extends AbstractValueBox<CheckBox, HTMLInputElement, Boole
    */
   public CheckBox(String label) {
     super("checkbox", label);
-    this.label = label;
     css("d-checkbox");
     setLabel(label);
     getInputElement().addEventListener("change", evt -> onCheck());
@@ -282,16 +280,16 @@ public class CheckBox extends AbstractValueBox<CheckBox, HTMLInputElement, Boole
     } else {
       getInputElement().setReadOnly(false);
       removeCss(READONLY);
-      getLabelTextElement().setTextContent(label);
+      getLabelTextElement().setTextContent(getLabel());
     }
     return this;
   }
 
   private void changeReadOnlyText() {
     if (isChecked()) {
-      getLabelTextElement().setTextContent(label + getCheckedReadonlyLabel());
+      getLabelTextElement().setTextContent(getLabel() + getCheckedReadonlyLabel());
     } else {
-      getLabelTextElement().setTextContent(label + getUnCheckedReadonlyLabel());
+      getLabelTextElement().setTextContent(getLabel() + getUnCheckedReadonlyLabel());
     }
   }
 

commit 6d3df90c59f3c36c3e9041bf5df891f396b29cad

fix #555 Regression from #540 , checkbox now recursivly append the check/uncheck labels when setReadOnly changed

domino-ui/src/main/java/org/dominokit/domino/ui/forms/CheckBox.java
@@ -17,11 +17,9 @@ package org.dominokit.domino.ui.forms;
 
 import static java.util.Objects.isNull;
 import static org.jboss.elemento.Elements.input;
+import static org.jboss.elemento.Elements.span;
 
-import elemental2.dom.Event;
-import elemental2.dom.EventListener;
-import elemental2.dom.HTMLAnchorElement;
-import elemental2.dom.HTMLInputElement;
+import elemental2.dom.*;
 import org.dominokit.domino.ui.keyboard.KeyboardEvents;
 import org.dominokit.domino.ui.style.Color;
 import org.dominokit.domino.ui.utils.Checkable;
@@ -36,6 +34,7 @@ public class CheckBox extends AbstractValueBox<CheckBox, HTMLInputElement, Boole
   private Color color;
   private String checkedReadonlyLabel = "Yes";
   private String unCheckedReadonlyLabel = "No";
+  private DominoElement<HTMLElement> readOnlyLabelElement = DominoElement.of(span());
 
   /** Creates a checkbox without a label */
   public CheckBox() {
@@ -287,17 +286,19 @@ public class CheckBox extends AbstractValueBox<CheckBox, HTMLInputElement, Boole
     } else {
       getInputElement().setReadOnly(false);
       removeCss(READONLY);
-      getLabelTextElement().setTextContent(getLabel());
+      readOnlyLabelElement.remove();
     }
     return this;
   }
 
   private void changeReadOnlyText() {
+    readOnlyLabelElement.remove();
     if (isChecked()) {
-      getLabelTextElement().setTextContent(getLabel() + getCheckedReadonlyLabel());
+      readOnlyLabelElement.setTextContent(getCheckedReadonlyLabel());
     } else {
-      getLabelTextElement().setTextContent(getLabel() + getUnCheckedReadonlyLabel());
+      readOnlyLabelElement.setTextContent(getUnCheckedReadonlyLabel());
     }
+    getLabelElement().appendChild(readOnlyLabelElement);
   }
 
   private String getCheckedReadonlyLabel() {