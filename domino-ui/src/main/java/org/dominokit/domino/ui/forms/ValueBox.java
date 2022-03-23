e309e3436cbd19b72813fa345f667274b4a00716
Ahmad K. Bawaneh <ahmad.bawaneh@dominokit.com>
2022-03-23 14:33:21 +0300

    fix: #644 for label is set to null if the label is not set using set label instead of factory/constructor

--- domino-ui/src/main/java/org/dominokit/domino/ui/forms/ValueBox.java
+++ domino-ui/src/main/java/org/dominokit/domino/ui/forms/ValueBox.java
@@ -243,7 +243,9 @@ public abstract class ValueBox<T extends ValueBox<T, E, V>, E extends HTMLElemen
   protected void linkLabelToField() {
     getLabelElement()
         .ifPresent(
-            labelElement -> labelElement.setAttribute("for", DominoElement.of(getInputElement()).getAttribute("id")));
+            labelElement ->
+                labelElement.setAttribute(
+                    "for", DominoElement.of(getInputElement()).getAttribute("id")));
   }
 
   /** manually call the change handlers if they are not paused */
