97f75f588c34b54e9f9b0a656a6e2aeb0f81b42f
Ahmad K. Bawaneh <ahmad.bawaneh@dominokit.com>
2022-06-25 12:42:55 +0300

    fix #665 Select dropdown menu becomes detached when enclosing Window is moved

--- domino-ui/src/main/java/org/dominokit/domino/ui/modals/Window.java
+++ domino-ui/src/main/java/org/dominokit/domino/ui/modals/Window.java
@@ -19,6 +19,7 @@ import static org.dominokit.domino.ui.style.Unit.px;
 
 import elemental2.dom.*;
 import jsinterop.base.Js;
+import org.dominokit.domino.ui.dropdown.DropDownMenu;
 import org.dominokit.domino.ui.grid.flex.FlexItem;
 import org.dominokit.domino.ui.grid.flex.FlexLayout;
 import org.dominokit.domino.ui.icons.Icons;
@@ -26,6 +27,7 @@ import org.dominokit.domino.ui.icons.MdiIcon;
 import org.dominokit.domino.ui.style.Calc;
 import org.dominokit.domino.ui.style.Color;
 import org.dominokit.domino.ui.utils.DominoElement;
+import org.dominokit.domino.ui.utils.PopupsCloser;
 import org.jboss.elemento.EventType;
 
 /**
@@ -308,6 +310,8 @@ public class Window extends BaseModal<Window> {
             EventType.mousedown,
             evt -> {
               if (draggable) {
+                DropDownMenu.closeAllMenus();
+                PopupsCloser.close();
                 MouseEvent mouseEvent = Js.uncheckedCast(evt);
                 if (!startMoving && mouseEvent.button == 0) {
                   mouseEvent.stopPropagation();
