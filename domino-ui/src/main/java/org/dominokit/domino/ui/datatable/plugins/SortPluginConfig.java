60a562da17bfc9a399ae8a9ac5b15733213b3d16
Ahmad K. Bawaneh <ahmad.bawaneh@dominokit.com>
2023-03-01 22:57:53 +0300

    fixes #768 DataTable SortPlugin to allow configurable icons

--- domino-ui/src/main/java/org/dominokit/domino/ui/datatable/plugins/SortPluginConfig.java
+++ domino-ui/src/main/java/org/dominokit/domino/ui/datatable/plugins/SortPluginConfig.java
@@ -15,8 +15,18 @@
  */
 package org.dominokit.domino.ui.datatable.plugins;
 
+import static java.util.Objects.nonNull;
+
+import java.util.function.Supplier;
+import org.dominokit.domino.ui.icons.BaseIcon;
+import org.dominokit.domino.ui.icons.Icons;
+
 public class SortPluginConfig implements PluginConfig {
   private boolean triStateSort = false;
+  private Supplier<BaseIcon<?>> ascendingIcon = Icons.ALL::sort_ascending_mdi;
+  private Supplier<BaseIcon<?>> descendingIcon = Icons.ALL::sort_descending_mdi;
+  private Supplier<BaseIcon<?>> unsortedIcon = Icons.ALL::sort_mdi;
+  private boolean showIconOnSortedColumnOnly = false;
 
   public boolean isTriStateSort() {
     return triStateSort;
@@ -24,6 +34,48 @@ public class SortPluginConfig implements PluginConfig {
 
   public SortPluginConfig setTriStateSort(boolean triStateSort) {
     this.triStateSort = triStateSort;
+    return this;
+  }
+
+  public Supplier<BaseIcon<?>> getAscendingIcon() {
+    return ascendingIcon;
+  }
+
+  public SortPluginConfig setAscendingIcon(Supplier<BaseIcon<?>> ascendingIcon) {
+    if (nonNull(ascendingIcon)) {
+      this.ascendingIcon = ascendingIcon;
+    }
+    return this;
+  }
+
+  public Supplier<BaseIcon<?>> getDescendingIcon() {
+    return descendingIcon;
+  }
+
+  public SortPluginConfig setDescendingIcon(Supplier<BaseIcon<?>> descendingIcon) {
+    if (nonNull(descendingIcon)) {
+      this.descendingIcon = descendingIcon;
+    }
+    return this;
+  }
+
+  public Supplier<BaseIcon<?>> getUnsortedIcon() {
+    return unsortedIcon;
+  }
+
+  public SortPluginConfig setUnsortedIcon(Supplier<BaseIcon<?>> unsortedIcon) {
+    if (nonNull(unsortedIcon)) {
+      this.unsortedIcon = unsortedIcon;
+    }
+    return this;
+  }
+
+  public boolean isShowIconOnSortedColumnOnly() {
+    return showIconOnSortedColumnOnly;
+  }
+
+  public SortPluginConfig setShowIconOnSortedColumnOnly(boolean showIconOnSortedColumnOnly) {
+    this.showIconOnSortedColumnOnly = showIconOnSortedColumnOnly;
     return this;
   }
 }
