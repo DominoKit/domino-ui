0a7d4bde01b4536e5a0cbe786a91a9822fb3e742
Ahmad K. Bawaneh <ahmad.bawaneh@dominokit.com>
2023-10-29 20:13:51 +0300

    fix #868 Scroll loading does not work when browser zoom is not 100%

--- domino-ui/src/main/java/org/dominokit/domino/ui/datatable/plugins/BodyScrollPlugin.java
+++ domino-ui/src/main/java/org/dominokit/domino/ui/datatable/plugins/BodyScrollPlugin.java
@@ -26,7 +26,10 @@ import org.dominokit.domino.ui.datatable.events.BodyScrollEvent;
  *
  * @param <T> the type of the data table records
  */
-public class BodyScrollPlugin<T> implements DataTablePlugin<T> {
+public class BodyScrollPlugin<T>
+    implements DataTablePlugin<T>, HasPluginConfig<T, BodyScrollPlugin<T>, BodyScrollPluginConfig> {
+
+  private BodyScrollPluginConfig config = new BodyScrollPluginConfig(0);
 
   /** {@inheritDoc} */
   @Override
@@ -44,10 +47,28 @@ public class BodyScrollPlugin<T> implements DataTablePlugin<T> {
           int clientHeight = new Double(scrollElement.clientHeight).intValue();
 
           if (JsMath.abs(offsetHeight) + JsMath.abs(scrollTop)
-              == new Double(scrollHeight + (offsetHeight - clientHeight)).intValue()) {
+              >= new Double(scrollHeight + (offsetHeight - clientHeight)).intValue()
+                  - config.getOffset()) {
             dataTable.fireTableEvent(new BodyScrollEvent(ScrollPosition.BOTTOM));
           }
         });
+  }
+
+  /**
+   * Sets up the plugin configuration.
+   *
+   * @param config The plugin configuration.
+   */
+  @Override
+  public BodyScrollPlugin<T> setConfig(BodyScrollPluginConfig config) {
+    this.config = config;
+    return this;
+  }
+
+  /** @return the plugin configuration */
+  @Override
+  public BodyScrollPluginConfig getConfig() {
+    return this.config;
   }
 
   /** An enum to specify the postion of the scroll */
