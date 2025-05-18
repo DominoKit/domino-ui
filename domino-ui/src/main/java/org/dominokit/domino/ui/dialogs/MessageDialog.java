18697caa82bd170e2ffb7e6e4afdf8a6aee75181
Hua Zhang <hzhang1978@gmail.com>
2025-05-18 12:01:51 +0200

    Fix in the setMessage method of MessageDialog: since messageElement is already a child of contentElement, calling appendChild directly is incorrect.

--- domino-ui/src/main/java/org/dominokit/domino/ui/dialogs/MessageDialog.java
+++ domino-ui/src/main/java/org/dominokit/domino/ui/dialogs/MessageDialog.java
@@ -130,8 +130,7 @@ public class MessageDialog extends AbstractDialog<MessageDialog> {
    * @return Current instance for chaining
    */
   public MessageDialog setMessage(String message) {
-    messageElement.remove();
-    appendChild(messageElement.get().setTextContent(message));
+    messageElement.get().setTextContent(message);
     return this;
   }
 
