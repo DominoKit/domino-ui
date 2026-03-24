b963cff0ab2dc89144022e17cfdbe1195cbdb528
Ahmad K. Bawaneh <ahmad.bawaneh@dominokit.com>
2026-03-24 21:24:33 +0300

    fix #1131 Allow the ability to make elements resizable mainly for window and modals.

---
+++ domino-ui/src/main/java/org/dominokit/domino/ui/utils/ElementResizerStyles.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright © 2019 Dominokit
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.dominokit.domino.ui.utils;
+
+import org.dominokit.domino.ui.style.CssClass;
+
+/** Constants for element resizer css classes names. */
+public interface ElementResizerStyles {
+
+  CssClass dui_element_resizer = () -> "dui-element-resizer";
+  CssClass dui_element_resizer_handle = () -> "dui-element-resizer-handle";
+  CssClass dui_element_resizer_top = () -> "dui-element-resizer-top";
+  CssClass dui_element_resizer_bottom = () -> "dui-element-resizer-bottom";
+  CssClass dui_element_resizer_left = () -> "dui-element-resizer-left";
+  CssClass dui_element_resizer_right = () -> "dui-element-resizer-right";
+  CssClass dui_element_resizer_corner = () -> "dui-element-resizer-corner";
+}
