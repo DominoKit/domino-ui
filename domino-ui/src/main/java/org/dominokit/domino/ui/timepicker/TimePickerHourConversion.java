997758c4ea7704fa76853faf651abb9a06d5227a
Ahmad K. Bawaneh <ahmad.bawaneh@dominokit.com>
2026-06-08 22:37:03 +0300

    fix #1136 Datepicker 12AM not selectable

---
+++ domino-ui/src/main/java/org/dominokit/domino/ui/timepicker/TimePickerHourConversion.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright © 2019 Dominokit
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.dominokit.domino.ui.timepicker;
+
+/** Utility methods for converting between 12-hour display values and Date hours. */
+final class TimePickerHourConversion {
+
+  private TimePickerHourConversion() {}
+
+  static int toDisplayHour(int hour) {
+    int normalizedHour = hour % 12;
+    return normalizedHour == 0 ? 12 : normalizedHour;
+  }
+
+  static TimePeriod toTimePeriod(int hour) {
+    return hour >= 12 ? TimePeriod.PM : TimePeriod.AM;
+  }
+
+  static int toDateHour(int displayHour, TimePeriod timePeriod) {
+    if (timePeriod == TimePeriod.PM) {
+      return displayHour == 12 ? 12 : displayHour + 12;
+    }
+    return displayHour == 12 ? 0 : displayHour;
+  }
+}
